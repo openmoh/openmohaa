35b767402b968e4218516da431780ca1704c06f4
L <private@private.com>
2023-02-05 13:27:12 +0100

    Indent

--- code/sys/win_bounds.cpp
+++ code/sys/win_bounds.cpp
@@ -23,10 +23,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // win_bounds.cpp : Bounds checks all new's and delete's
 
 extern "C"
-   {
-   void *b_malloc ( unsigned int size );
-   void  b_free ( void * );
-   };
+{
+	void* b_malloc(unsigned int size);
+	void  b_free(void*);
+};
 
 //////////////////////////
 // Bounds checking
@@ -66,15 +66,15 @@ extern "C"
 
 #include <malloc.h>
 
-void *b_malloc ( unsigned int size )
-   {
-   return malloc ( size );
-   }
+void* b_malloc(unsigned int size)
+{
+	return malloc(size);
+}
 
-void b_free ( void *what )
-   {
-   free ( what );
-   }
+void b_free(void* what)
+{
+	free(what);
+}
 
 ///////////////////////////
 // If bounds are enabled
@@ -89,7 +89,7 @@ void b_free ( void *what )
 typedef struct {
 	unsigned int header;
 	int size;
-	void *returned;
+	void* returned;
 } bounds_type_t;
 
 #define PAGE_SIZE ( 4096 )
@@ -97,93 +97,93 @@ typedef struct {
 #define NORMAL_HEADER 0xdeadbeef
 #define ARRAY_HEADER 0xdeadbabe
 
-static unsigned int bounds_numpages ( unsigned int size ) {
+static unsigned int bounds_numpages(unsigned int size) {
 	unsigned int ret;
 
 	ret = size / PAGE_SIZE + 3;
-	if ( size % PAGE_SIZE )
+	if (size % PAGE_SIZE)
 		ret++;
 
 	return ret;
 }
 
-void *bounds_malloc ( unsigned int size, unsigned head = NORMAL_HEADER ) {
-	bounds_type_t *where;
+void* bounds_malloc(unsigned int size, unsigned head = NORMAL_HEADER) {
+	bounds_type_t* where;
 	unsigned int num_pages;
-	void *mainaddress;
+	void* mainaddress;
 
-	num_pages = bounds_numpages ( size );
+	num_pages = bounds_numpages(size);
 
-	mainaddress = VirtualAlloc ( NULL, num_pages * PAGE_SIZE, MEM_RESERVE, PAGE_NOACCESS );
-	VirtualAlloc ( mainaddress, PAGE_SIZE, MEM_COMMIT, PAGE_READWRITE );
-	VirtualAlloc ( (char *) mainaddress + PAGE_SIZE * 2, ( num_pages - 3 ) * PAGE_SIZE, MEM_COMMIT, PAGE_READWRITE );
-	where = (bounds_type_t *) mainaddress;
+	mainaddress = VirtualAlloc(NULL, num_pages * PAGE_SIZE, MEM_RESERVE, PAGE_NOACCESS);
+	VirtualAlloc(mainaddress, PAGE_SIZE, MEM_COMMIT, PAGE_READWRITE);
+	VirtualAlloc((char*)mainaddress + PAGE_SIZE * 2, (num_pages - 3) * PAGE_SIZE, MEM_COMMIT, PAGE_READWRITE);
+	where = (bounds_type_t*)mainaddress;
 
 	where->header = head;
 	where->size = size;
 #ifdef BOUNDS_ENDOFPAGE
-	where->returned = (char *) mainaddress + ( num_pages - 1 ) * PAGE_SIZE - size;
+	where->returned = (char*)mainaddress + (num_pages - 1) * PAGE_SIZE - size;
 #else
-	where->returned = (char *) mainaddress + PAGE_SIZE * 2;
+	where->returned = (char*)mainaddress + PAGE_SIZE * 2;
 #endif
-   memset ( where->returned, 0xdc, size );
-   
-   return where->returned;
+	memset(where->returned, 0xdc, size);
+
+	return where->returned;
 }
 
-void bounds_free ( void *address, unsigned head = NORMAL_HEADER ) {
-	bounds_type_t *where;
+void bounds_free(void* address, unsigned head = NORMAL_HEADER) {
+	bounds_type_t* where;
 	unsigned int num_pages;
-	void *mainaddress;
+	void* mainaddress;
 
-	mainaddress = (char *) address - PAGE_SIZE * 2;
+	mainaddress = (char*)address - PAGE_SIZE * 2;
 #ifdef BOUNDS_ENDOFPAGE
-	mainaddress = (char *) mainaddress - ( (unsigned int) mainaddress % 4096 );
+	mainaddress = (char*)mainaddress - ((unsigned int)mainaddress % 4096);
 #endif
 
-	where = (bounds_type_t *) mainaddress;
+	where = (bounds_type_t*)mainaddress;
 
-	if ( where->header != head || where->returned != address ) {
+	if (where->header != head || where->returned != address) {
 		__asm int 3 // Breakpoint
 	}
-	num_pages = bounds_numpages ( where->size );
+	num_pages = bounds_numpages(where->size);
 
 	// All pages must be in the same state to be MEM_RELEASED
-	VirtualFree ( mainaddress, PAGE_SIZE, MEM_DECOMMIT );
-	VirtualFree ( (char *) mainaddress + PAGE_SIZE * 2, (num_pages - 3 ) * PAGE_SIZE, MEM_DECOMMIT );
-	VirtualFree ( mainaddress, 0, MEM_RELEASE );
+	VirtualFree(mainaddress, PAGE_SIZE, MEM_DECOMMIT);
+	VirtualFree((char*)mainaddress + PAGE_SIZE * 2, (num_pages - 3) * PAGE_SIZE, MEM_DECOMMIT);
+	VirtualFree(mainaddress, 0, MEM_RELEASE);
 }
 
-void *b_malloc ( unsigned int size )
-   {
-   return bounds_malloc ( size );   
-   }
+void* b_malloc(unsigned int size)
+{
+	return bounds_malloc(size);
+}
 
-void b_free ( void *what )
-   {
-   bounds_free ( what );
-   }
+void b_free(void* what)
+{
+	bounds_free(what);
+}
 
-void *operator new[] ( size_t size )
-   {
-   return bounds_malloc ( size, ARRAY_HEADER );
-   }
+void* operator new[](size_t size)
+{
+	return bounds_malloc(size, ARRAY_HEADER);
+}
 
-void *operator new ( size_t size )
-   {
-   return bounds_malloc ( size );
-   }
+void* operator new (size_t size)
+{
+	return bounds_malloc(size);
+}
 
-void operator delete [] ( void *what )
-   {
-   if ( what )
-      bounds_free ( what, ARRAY_HEADER );
-   }
+void operator delete [](void* what)
+{
+	if (what)
+		bounds_free(what, ARRAY_HEADER);
+}
 
-void operator delete ( void *what )
-   {
-   if ( what )
-      bounds_free ( what );
-   }
+void operator delete (void* what)
+{
+	if (what)
+		bounds_free(what);
+}
 
 #endif /* !DISABLE_BOUNDS */
