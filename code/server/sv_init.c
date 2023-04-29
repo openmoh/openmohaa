d48f252bb0aee918d71b248819d771404e0cba07
L <private@private.com>
2023-04-30 01:09:57 +0200

    Include tiki.h

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -22,6 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "server.h"
 #include "../client/client.h"
+#include "tiki.h"
 
 static char last_mapname[ MAX_QPATH ];
 static int g_iSvsTimeFixupCount;
