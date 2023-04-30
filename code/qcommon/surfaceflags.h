9df167cea134fd0f1777d839c84860d258649911
L <private@private.com>
2023-04-30 14:53:35 +0200

    Added MASK_SURF_TYPE

--- code/qcommon/surfaceflags.h
+++ code/qcommon/surfaceflags.h
@@ -117,3 +117,5 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define	SURF_METALSTEPS			0x1000	// clanking footsteps
 #define	SURF_LIGHTFILTER		0x8000	// act as a light filter during q3map -light
 #define SURF_DUST				0x40000 // leave a dust trail when walking on this surface
+
+#define MASK_SURF_TYPE        (SURF_TYPE_WOOD|SURF_TYPE_METAL|SURF_TYPE_ROCK|SURF_TYPE_DIRT|SURF_TYPE_GRILL|SURF_TYPE_ORGANIC|SURF_TYPE_SQUISHY)
