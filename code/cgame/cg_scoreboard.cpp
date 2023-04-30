dbe84287891913028e71a9341a4c8e8759119e66
L <private@private.com>
2023-05-01 00:09:27 +0200

    Added scoreboard functions stub

--- code/cgame/cg_scoreboard.cpp
+++ code/cgame/cg_scoreboard.cpp
@@ -22,3 +22,68 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 // DESCRIPTION:
 // Scoreboard
+
+#include "cg_local.h"
+
+void CG_GetScoreBoardColor(float* fR, float* fG, float* fB, float* fA)
+{
+    // FIXME: unimplemented
+}
+
+void CG_GetScoreBoardFontColor(float* fR, float* fG, float* fB, float* fA)
+{
+    // FIXME: unimplemented
+}
+
+void CG_GetScoreBoardPosition(float* fX, float* fY, float* fW, float* fH)
+{
+    // FIXME: unimplemented
+}
+
+int CG_GetScoreBoardDrawHeader()
+{
+    // FIXME: unimplemented
+    return 0;
+}
+
+const char* CG_GetColumnName(int iColumnNum, int* iColumnWidth)
+{
+    int iReturnWidth;
+    const char* pszReturnString;
+
+    switch (iColumnNum) {
+    case 0:
+        iReturnWidth = 128;
+        pszReturnString = "Name";
+        break;
+    case 1:
+        iReturnWidth = 64;
+        pszReturnString = "Kills";
+        break;
+    case 2:
+        iReturnWidth = 64;
+        pszReturnString = "Deaths";
+        if (cgs.gametype > GT_TEAM) {
+            pszReturnString = "Total";
+        }
+        break;
+    case 3:
+        iReturnWidth = 64;
+        pszReturnString = "Time";
+        break;
+    case 4:
+        iReturnWidth = 64;
+        pszReturnString = "Ping";
+        break;
+    default:
+        iReturnWidth = 0;
+        pszReturnString = 0;
+        break;
+    }
+
+    if (iColumnWidth) {
+        *iColumnWidth = iReturnWidth;
+    }
+
+    return pszReturnString;
+}
