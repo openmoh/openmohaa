1b0bc7c104748955f1b7783ad38aca491409cde2
L <private@private.com>
2023-05-04 20:29:43 +0200

    Added bullet tracers

--- misc/restuff/ida/ida_cgame.h
+++ misc/restuff/ida/ida_cgame.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include "ida_common.h"
+
+typedef struct {
+  vec3_t i_vBarrel;
+  vec3_t i_vStart;
+  vec3_t *i_vEnd;
+  int i_iNumBullets;
+  qboolean iLarge;
+  int iTracerVisible;
+  qboolean bIgnoreEntities;
+} bullet_tracer_t;
+
+#define MAX_TRACERS 32
+#define MAX_BULLETS 1024
+#define MAX_IMPACTS 64
