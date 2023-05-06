99f886f350afda8f00fc43aef9ccec6d80cb86fa
L <private@private.com>
2023-05-06 19:10:28 +0200

    Implemented UI commands

--- code/cgame/cg_ui.cpp
+++ code/cgame/cg_ui.cpp
@@ -24,70 +24,173 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // UI features
 
 #include "cg_local.h"
+#include "str.h"
 
 void CG_MessageMode_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cgi.UI_ToggleDMMessageConsole(300);
 }
 
 void CG_MessageMode_All_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cgi.UI_ToggleDMMessageConsole(100);
 }
 
 void CG_MessageMode_Team_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cgi.UI_ToggleDMMessageConsole(200);
 }
 
 void CG_MessageMode_Private_f(void)
 {
-    // FIXME: unimplemented
+    int clientNum;
+
+    if (!cgs.gametype) {
+        return;
+    }
+
+    clientNum = atoi(cgi.Argv(1)) - 1;
+    if (clientNum < 0 || clientNum >= MAX_CLIENTS)
+    {
+        cgi.Printf(HUD_MESSAGE_CHAT_WHITE "Message Error: %s is a bad client number\n", cgi.Argv(1));
+        return;
+    }
+
+    cgi.UI_ToggleDMMessageConsole(clientNum);
 }
 
 void CG_MessageSingleAll_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    if (cgi.Argc() > 1) {
+        cgi.SendClientCommand(va("dmmessage 0 %s\n", cgi.Args()));
+    }
+    else {
+        cgi.UI_ToggleDMMessageConsole(-100);
+    }
 }
 
 void CG_MessageSingleTeam_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    if (cgi.Argc() > 1) {
+        cgi.SendClientCommand(va("dmmessage -1 %s\n", cgi.Args()));
+    }
+    else {
+        cgi.UI_ToggleDMMessageConsole(-200);
+    }
 }
 
 void CG_MessageSingleClient_f(void)
 {
-    // FIXME: unimplemented
+    int clientNum;
+
+    if (!cgs.gametype) {
+        return;
+    }
+
+    clientNum = atoi(cgi.Argv(1));
+    if (clientNum < 1 || clientNum > MAX_CLIENTS)
+    {
+        cgi.Printf(HUD_MESSAGE_CHAT_WHITE "Message Error: %s is a bad client number\n", cgi.Argv(1));
+        return;
+    }
+
+    if (cgi.Argc() > 2)
+    {
+        int i;
+        str sString;
+
+        sString = "dmmessage ";
+        sString += va("%i", clientNum);
+
+        // copy the rest
+        for (i = 2; i < cgi.Argc(); i++) {
+            sString += va("%s", cgi.Argv(i));
+        }
+
+        sString += "\n";
+        cgi.SendClientCommand(sString.c_str());
+    }
+    else {
+        cgi.UI_ToggleDMMessageConsole(-clientNum);
+    }
 }
 
 void CG_InstaMessageMain_f(void)
 {
-    // FIXME: unimplemented
+    if (!voiceChat->integer) {
+        return;
+    }
+
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cg.iInstaMessageMenu = -1;
 }
 
 void CG_InstaMessageGroupA_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cg.iInstaMessageMenu = 1;
 }
 
 void CG_InstaMessageGroupB_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cg.iInstaMessageMenu = 2;
 }
 
 void CG_InstaMessageGroupC_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cg.iInstaMessageMenu = 3;
 }
 
 void CG_InstaMessageGroupD_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cg.iInstaMessageMenu = 4;
 }
 
 void CG_InstaMessageGroupE_f(void)
 {
-    // FIXME: unimplemented
+    if (!cgs.gametype) {
+        return;
+    }
+
+    cg.iInstaMessageMenu = 5;
 }
 
 void CG_HudPrint_f(void)
