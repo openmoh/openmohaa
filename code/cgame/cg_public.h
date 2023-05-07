6c3a4e7a18cbc14d5874b1df1abaab1b1a011fb8
L <private@private.com>
2023-05-07 19:36:53 +0200

    const char* param fix

--- code/cgame/cg_public.h
+++ code/cgame/cg_public.h
@@ -250,7 +250,7 @@ typedef struct
                                           float s1, float t1, float s2, float t2, 
                                         qhandle_t hShader );    // 0 = white
     fontheader_t* (*R_LoadFont)(const char* name);
-    void            (*R_DrawString)(fontheader_t* font, char* text, float x, float y, int maxLen, qboolean virtualScreen);
+    void            (*R_DrawString)(fontheader_t* font, const char* text, float x, float y, int maxLen, qboolean virtualScreen);
    refEntity_t *  (*R_GetRenderEntity)( int entityNumber );
    void              (*R_ModelBounds)( clipHandle_t model, vec3_t mins, vec3_t maxs );
    float           (*R_ModelRadius)( clipHandle_t model );
@@ -295,7 +295,7 @@ typedef struct
    // ==================== UI STUFF ==========================
    void             (*UI_ShowMenu)(char* name, qboolean bForce);
    void             (*UI_HideMenu)(char* name, qboolean bForce);
-   int              (*UI_FontStringWidth)(fontheader_t* font, char* string, int maxLen);
+   int              (*UI_FontStringWidth)(fontheader_t* font, const char* string, int maxLen);
 
    int              (*Key_StringToKeynum)(char* str);
    char*            (*Key_KeynumToBindString)(int keyNum);
