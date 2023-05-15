9abc35bc184554be638c58c6c253a529731609e3
OM <private@private.com>
2023-05-15 20:15:06 +0200

    Fixed colors not working properly (used floats instead of bytes)

--- code/renderer/tr_draw.c
+++ code/renderer/tr_draw.c
@@ -30,19 +30,19 @@ vec4_t r_colorWhite = { 1.0, 1.0, 1.0, 1.0 };
 Draw_SetColor
 ================
 */
-void Draw_SetColor( const vec4_t rgba ) {
+void Draw_SetColor(const vec4_t rgba) {
 #if 1
-	if( !rgba ) {
+	if (!rgba) {
 		rgba = r_colorWhite;
 	}
 
-	backEnd.color2D[ 0 ] = rgba[ 0 ] * tr.identityLight;
-	backEnd.color2D[ 1 ] = rgba[ 1 ] * tr.identityLight;
-	backEnd.color2D[ 2 ] = rgba[ 2 ] * tr.identityLight;
-	backEnd.color2D[ 3 ] = rgba[ 3 ];
-	qglColor4fv( backEnd.color2D );
+	backEnd.color2D[0] = (byte)(rgba[0] * tr.identityLightByte);
+	backEnd.color2D[1] = (byte)(rgba[1] * tr.identityLightByte);
+	backEnd.color2D[2] = (byte)(rgba[2] * tr.identityLightByte);
+	backEnd.color2D[3] = (byte)(rgba[3] * 255.0);
+	qglColor4ubv(backEnd.color2D);
 #else
-	RE_SetColor( rgba );
+	RE_SetColor(rgba);
 #endif
 }
 
@@ -51,42 +51,43 @@ void Draw_SetColor( const vec4_t rgba ) {
 Draw_StretchPic
 ================
 */
-void Draw_StretchPic( float x, float y, float w, float h, float s1, float t1, float s2, float t2, qhandle_t hShader ) {
+void Draw_StretchPic(float x, float y, float w, float h, float s1, float t1, float s2, float t2, qhandle_t hShader) {
 #if 1
-	shader_t	*shader;
+	shader_t* shader;
 
 	R_SyncRenderThread();
 
-	if( hShader ) {
-		shader = R_GetShaderByHandle( hShader );
-	} else {
+	if (hShader) {
+		shader = R_GetShaderByHandle(hShader);
+	}
+	else {
 		shader = tr.defaultShader;
 	}
 
-	if( w <= 0 ) {
-		w = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->width;
-		h = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->height;
+	if (w <= 0) {
+		w = shader->unfoggedStages[0]->bundle[0].image[0]->width;
+		h = shader->unfoggedStages[0]->bundle[0].image[0]->height;
 	}
 
 	// draw the pic
-	RB_Color4f( backEnd.color2D[ 0 ], backEnd.color2D[ 1 ], backEnd.color2D[ 2 ], backEnd.color2D[ 3 ] );
-	RB_BeginSurface( shader );
+	RB_Color4f(backEnd.color2D[0], backEnd.color2D[1], backEnd.color2D[2], backEnd.color2D[3]);
+	RB_BeginSurface(shader);
 
-	RB_Texcoord2f( s1, t1 );
-	RB_Vertex2f( x, y );
+	RB_Texcoord2f(s1, t1);
+	RB_Vertex2f(x, y);
 
-	RB_Texcoord2f( s2, t1 );
-	RB_Vertex2f( x + w, y );
+	RB_Texcoord2f(s2, t1);
+	RB_Vertex2f(x + w, y);
 
-	RB_Texcoord2f( s1, t2 );
-	RB_Vertex2f( x, y + h );
+	RB_Texcoord2f(s1, t2);
+	RB_Vertex2f(x, y + h);
 
-	RB_Texcoord2f( s2, t2 );
-	RB_Vertex2f( x + w, y + h );
+	RB_Texcoord2f(s2, t2);
+	RB_Vertex2f(x + w, y + h);
 
 	RB_StreamEnd();
 #else
-	RE_StretchPic( x, y, w, h, s1, t1, s2, t2, hShader );
+	RE_StretchPic(x, y, w, h, s1, t1, s2, t2, hShader);
 #endif
 }
 
@@ -95,43 +96,43 @@ void Draw_StretchPic( float x, float y, float w, float h, float s1, float t1, fl
 Draw_TilePic
 ================
 */
-void Draw_TilePic( float x, float y, float w, float h , qhandle_t hShader ) {
-	shader_t	*shader;
+void Draw_TilePic(float x, float y, float w, float h, qhandle_t hShader) {
+	shader_t* shader;
 	float		picw, pich;
 
 	R_SyncRenderThread();
 
-	if( hShader ) {
-		shader = R_GetShaderByHandle( hShader );
+	if (hShader) {
+		shader = R_GetShaderByHandle(hShader);
 	}
 	else {
 		shader = tr.defaultShader;
 	}
 
-	if( w <= 0 ) {
-		w = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->width;
-		h = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->height;
+	if (w <= 0) {
+		w = shader->unfoggedStages[0]->bundle[0].image[0]->width;
+		h = shader->unfoggedStages[0]->bundle[0].image[0]->height;
 	}
 
-	picw = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->uploadWidth;
-	pich = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->uploadHeight;
+	picw = shader->unfoggedStages[0]->bundle[0].image[0]->uploadWidth;
+	pich = shader->unfoggedStages[0]->bundle[0].image[0]->uploadHeight;
 
 	// draw the pic
-	RB_Color4f( backEnd.color2D[ 0 ], backEnd.color2D[ 1 ], backEnd.color2D[ 2 ], backEnd.color2D[ 3 ] );
+	RB_Color4f(backEnd.color2D[0], backEnd.color2D[1], backEnd.color2D[2], backEnd.color2D[3]);
 
-	RB_StreamBegin( shader );
+	RB_StreamBegin(shader);
 
-	RB_Texcoord2f( x / picw, y / pich );
-	RB_Vertex2f( x, y );
+	RB_Texcoord2f(x / picw, y / pich);
+	RB_Vertex2f(x, y);
 
-	RB_Texcoord2f( ( x + w ) / picw, y / pich );
-	RB_Vertex2f( x + w, y );
+	RB_Texcoord2f((x + w) / picw, y / pich);
+	RB_Vertex2f(x + w, y);
 
-	RB_Texcoord2f( x / picw, ( y + h ) / pich );
-	RB_Vertex2f( x, y + h );
+	RB_Texcoord2f(x / picw, (y + h) / pich);
+	RB_Vertex2f(x, y + h);
 
-	RB_Texcoord2f( ( x + w ) / picw, ( y + h ) / pich );
-	RB_Vertex2f( x + w, y + h );
+	RB_Texcoord2f((x + w) / picw, (y + h) / pich);
+	RB_Vertex2f(x + w, y + h);
 
 	RB_StreamEnd();
 }
@@ -141,42 +142,43 @@ void Draw_TilePic( float x, float y, float w, float h , qhandle_t hShader ) {
 Draw_TilePicOffset
 ================
 */
-void Draw_TilePicOffset( float x, float y, float w, float h, qhandle_t hShader, int offsetX, int offsetY ) {
-	shader_t	*shader;
+void Draw_TilePicOffset(float x, float y, float w, float h, qhandle_t hShader, int offsetX, int offsetY) {
+	shader_t* shader;
 	float		picw, pich;
 
 	R_SyncRenderThread();
 
-	if( hShader ) {
-		shader = R_GetShaderByHandle( hShader );
-	} else {
+	if (hShader) {
+		shader = R_GetShaderByHandle(hShader);
+	}
+	else {
 		shader = tr.defaultShader;
 	}
 
-	if( w <= 0 ) {
-		w = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->width;
-		h = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->height;
+	if (w <= 0) {
+		w = shader->unfoggedStages[0]->bundle[0].image[0]->width;
+		h = shader->unfoggedStages[0]->bundle[0].image[0]->height;
 	}
 
-	picw = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->uploadWidth;
-	pich = shader->unfoggedStages[ 0 ]->bundle[ 0 ].image[ 0 ]->uploadHeight;
+	picw = shader->unfoggedStages[0]->bundle[0].image[0]->uploadWidth;
+	pich = shader->unfoggedStages[0]->bundle[0].image[0]->uploadHeight;
 
 	// draw the pic
-	RB_Color4f( backEnd.color2D[ 0 ], backEnd.color2D[ 1 ], backEnd.color2D[ 2 ], backEnd.color2D[ 3 ] );
+	RB_Color4f(backEnd.color2D[0], backEnd.color2D[1], backEnd.color2D[2], backEnd.color2D[3]);
 
-	RB_StreamBegin( shader );
+	RB_StreamBegin(shader);
 
-	RB_Texcoord2f( x / picw, y / pich );
-	RB_Vertex2f( x + offsetX, y + offsetY );
+	RB_Texcoord2f(x / picw, y / pich);
+	RB_Vertex2f(x + offsetX, y + offsetY);
 
-	RB_Texcoord2f( ( x + w ) / picw, y / pich );
-	RB_Vertex2f( x + offsetX + w, y + offsetY );
+	RB_Texcoord2f((x + w) / picw, y / pich);
+	RB_Vertex2f(x + offsetX + w, y + offsetY);
 
-	RB_Texcoord2f( x / picw, ( y + h ) / pich );
-	RB_Vertex2f( x + offsetX, y + offsetY + h );
+	RB_Texcoord2f(x / picw, (y + h) / pich);
+	RB_Vertex2f(x + offsetX, y + offsetY + h);
 
-	RB_Texcoord2f( ( x + w ) / picw, ( y + h ) / pich );
-	RB_Vertex2f( x + offsetX + w, y + offsetY + h );
+	RB_Texcoord2f((x + w) / picw, (y + h) / pich);
+	RB_Vertex2f(x + offsetX + w, y + offsetY + h);
 
 	RB_StreamEnd();
 }
@@ -186,26 +188,27 @@ void Draw_TilePicOffset( float x, float y, float w, float h, qhandle_t hShader, 
 Draw_TrianglePic
 ================
 */
-void Draw_TrianglePic( const vec2_t vPoints[ 3 ], const vec2_t vTexCoords[ 3 ], qhandle_t hShader ) {
+void Draw_TrianglePic(const vec2_t vPoints[3], const vec2_t vTexCoords[3], qhandle_t hShader) {
 	int			i;
-	shader_t	*shader;
+	shader_t* shader;
 
 	R_SyncRenderThread();
 
-	if( hShader ) {
-		shader = R_GetShaderByHandle( hShader );
-	} else {
+	if (hShader) {
+		shader = R_GetShaderByHandle(hShader);
+	}
+	else {
 		shader = tr.defaultShader;
 	}
 
 	// draw the pic
-	RB_Color4f( backEnd.color2D[ 0 ], backEnd.color2D[ 1 ], backEnd.color2D[ 2 ], backEnd.color2D[ 3 ] );
+	RB_Color4f(backEnd.color2D[0], backEnd.color2D[1], backEnd.color2D[2], backEnd.color2D[3]);
 
-	RB_BeginSurface( shader );
+	RB_BeginSurface(shader);
 
-	for( i = 0; i < 3; i++ ) {
-		RB_Texcoord2f( vTexCoords[ i ][ 0 ], vTexCoords[ i ][ 1 ] );
-		RB_Vertex2f( vPoints[ i ][ 0 ], vPoints[ i ][ 1 ] );
+	for (i = 0; i < 3; i++) {
+		RB_Texcoord2f(vTexCoords[i][0], vTexCoords[i][1]);
+		RB_Vertex2f(vPoints[i][0], vPoints[i][1]);
 	}
 
 	RB_StreamEnd();
@@ -216,7 +219,7 @@ void Draw_TrianglePic( const vec2_t vPoints[ 3 ], const vec2_t vTexCoords[ 3 ], 
 RE_DrawBackground_TexSubImage
 ================
 */
-void RE_DrawBackground_TexSubImage( int cols, int rows, int bgr, byte *data ) {
+void RE_DrawBackground_TexSubImage(int cols, int rows, int bgr, byte* data) {
 	GLenum	format;
 	int		w, h;
 
@@ -226,45 +229,46 @@ void RE_DrawBackground_TexSubImage( int cols, int rows, int bgr, byte *data ) {
 	R_SyncRenderThread();
 	qglFinish();
 
-	if( bgr ) {
+	if (bgr) {
 		format = GL_BGR_EXT;
-	} else {
+	}
+	else {
 		format = GL_RGB;
 	}
 
-	GL_Bind( tr.scratchImage[ 0 ] );
+	GL_Bind(tr.scratchImage[0]);
 
-	if( cols == tr.scratchImage[ 0 ]->width && rows == tr.scratchImage[ 0 ]->height && format == tr.scratchImage[ 0 ]->internalFormat )
+	if (cols == tr.scratchImage[0]->width && rows == tr.scratchImage[0]->height && format == tr.scratchImage[0]->internalFormat)
 	{
-		qglTexSubImage2D( 3553, 0, 0, 0, cols, rows, format, 5121, data );
+		qglTexSubImage2D(3553, 0, 0, 0, cols, rows, format, 5121, data);
 	}
 	else
 	{
-		tr.scratchImage[ 0 ]->uploadWidth = cols;
-		tr.scratchImage[ 0 ]->uploadHeight = rows;
-		tr.scratchImage[ 0 ]->internalFormat = format;
-		qglTexImage2D( GL_TEXTURE_2D, 0, 3, cols, rows, 0, format, 5121, data );
-		qglTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, 9729.0 );
-		qglTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, 9729.0 );
+		tr.scratchImage[0]->uploadWidth = cols;
+		tr.scratchImage[0]->uploadHeight = rows;
+		tr.scratchImage[0]->internalFormat = format;
+		qglTexImage2D(GL_TEXTURE_2D, 0, 3, cols, rows, 0, format, 5121, data);
+		qglTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, 9729.0);
+		qglTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, 9729.0);
 	}
 
-	qglDisable( GL_CULL_FACE );
-	qglDisable( GL_DEPTH_TEST );
-	qglEnable( GL_TEXTURE_2D );
+	qglDisable(GL_CULL_FACE);
+	qglDisable(GL_DEPTH_TEST);
+	qglEnable(GL_TEXTURE_2D);
 
-	qglBegin( GL_QUADS );
+	qglBegin(GL_QUADS);
 
-	qglTexCoord2f( 0.5 / ( GLfloat )cols, ( ( GLfloat )rows - 0.5 ) / rows );
-	qglVertex2f( 0, 0 );
+	qglTexCoord2f(0.5 / (GLfloat)cols, ((GLfloat)rows - 0.5) / rows);
+	qglVertex2f(0, 0);
 
-	qglTexCoord2f( ( ( GLfloat )cols - 0.5 ) / cols, ( ( GLfloat )rows - 0.5 ) / rows );
-	qglVertex2f( w, 0 );
+	qglTexCoord2f(((GLfloat)cols - 0.5) / cols, ((GLfloat)rows - 0.5) / rows);
+	qglVertex2f(w, 0);
 
-	qglTexCoord2f( ( ( GLfloat )cols - 0.5 ) / cols, 0.5 / ( GLfloat )rows );
-	qglVertex2f( w, h );
+	qglTexCoord2f(((GLfloat)cols - 0.5) / cols, 0.5 / (GLfloat)rows);
+	qglVertex2f(w, h);
 
-	qglTexCoord2f( 0.5 / ( GLfloat )rows, 0.5 / ( GLfloat )rows );
-	qglVertex2f( 0, h );
+	qglTexCoord2f(0.5 / (GLfloat)rows, 0.5 / (GLfloat)rows);
+	qglVertex2f(0, h);
 
 	qglEnd();
 }
@@ -274,7 +278,7 @@ void RE_DrawBackground_TexSubImage( int cols, int rows, int bgr, byte *data ) {
 RE_DrawBackground_DrawPixels
 ================
 */
-void RE_DrawBackground_DrawPixels( int cols, int rows, int bgr, byte *data ) {
+void RE_DrawBackground_DrawPixels(int cols, int rows, int bgr, byte* data) {
 	// FIXME: stub
 }
 
@@ -283,23 +287,23 @@ void RE_DrawBackground_DrawPixels( int cols, int rows, int bgr, byte *data ) {
 AddBox
 ================
 */
-void AddBox( float x, float y, float w, float h ) {
+void AddBox(float x, float y, float w, float h) {
 	R_SyncRenderThread();
 
-	qglColor4fv( backEnd.color2D );
-	qglDisable( GL_TEXTURE_2D );
-	GL_State( 0x422 );
+	qglColor4ubv(backEnd.color2D);
+	qglDisable(GL_TEXTURE_2D);
+	GL_State(GLS_SET2D | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ONE);
 
-	qglBegin( GL_QUADS );
+	qglBegin(GL_QUADS);
 
-	qglVertex2f( x, y );
-	qglVertex2f( x + w, y );
-	qglVertex2f( x + w, y + h );
-	qglVertex2f( x, y + h );
+	qglVertex2f(x, y);
+	qglVertex2f(x + w, y);
+	qglVertex2f(x + w, y + h);
+	qglVertex2f(x, y + h);
 
 	qglEnd();
 
-	qglEnable( GL_TEXTURE_2D );
+	qglEnable(GL_TEXTURE_2D);
 }
 
 /*
@@ -307,23 +311,23 @@ void AddBox( float x, float y, float w, float h ) {
 DrawBox
 ================
 */
-void DrawBox( float x, float y, float w, float h ) {
+void DrawBox(float x, float y, float w, float h) {
 	R_SyncRenderThread();
 
-	qglColor4fv( backEnd.color2D );
-	qglDisable( GL_TEXTURE_2D );
-	GL_State( 0x465 );
+	qglColor4ubv(backEnd.color2D);
+	qglDisable(GL_TEXTURE_2D);
+	GL_State(GLS_SET2D | GLS_DSTBLEND_ONE_MINUS_SRC_ALPHA | GLS_SRCBLEND_SRC_ALPHA);
 
-	qglBegin( GL_QUADS );
+	qglBegin(GL_QUADS);
 
-	qglVertex2f( x, y );
-	qglVertex2f( x + w, y );
-	qglVertex2f( x + w, y + h );
-	qglVertex2f( x, y + h );
+	qglVertex2f(x, y);
+	qglVertex2f(x + w, y);
+	qglVertex2f(x + w, y + h);
+	qglVertex2f(x, y + h);
 
 	qglEnd();
 
-	qglEnable( GL_TEXTURE_2D );
+	qglEnable(GL_TEXTURE_2D);
 }
 
 /*
@@ -331,30 +335,30 @@ void DrawBox( float x, float y, float w, float h ) {
 DrawLineLoop
 ================
 */
-void DrawLineLoop( const vec2_t *points, int count, int stipple_factor, int stipple_mask ) {
+void DrawLineLoop(const vec2_t* points, int count, int stipple_factor, int stipple_mask) {
 	int		i;
 
 	R_SyncRenderThread();
 
-	qglDisable( GL_TEXTURE_2D );
+	qglDisable(GL_TEXTURE_2D);
 
-	if( stipple_factor ) {
-		qglEnable( GL_LINE_STIPPLE );
-		qglLineStipple( stipple_factor, stipple_mask );
+	if (stipple_factor) {
+		qglEnable(GL_LINE_STIPPLE);
+		qglLineStipple(stipple_factor, stipple_mask);
 	}
 
-	qglBegin( GL_LINE_LOOP );
+	qglBegin(GL_LINE_LOOP);
 
-	for( i = 0; i < count; i++ ) {
-		qglVertex2i( points[ i ][ 0 ], points[ i ][ 1 ] );
+	for (i = 0; i < count; i++) {
+		qglVertex2i(points[i][0], points[i][1]);
 	}
 
 	qglEnd();
 
-	qglEnable( GL_TEXTURE_2D );
+	qglEnable(GL_TEXTURE_2D);
 
-	if( stipple_factor ) {
-		qglDisable( GL_LINE_STIPPLE );
+	if (stipple_factor) {
+		qglDisable(GL_LINE_STIPPLE);
 	}
 }
 
@@ -363,7 +367,7 @@ void DrawLineLoop( const vec2_t *points, int count, int stipple_factor, int stip
 Set2DWindow
 ================
 */
-void Set2DWindow( int x, int y, int w, int h, float left, float right, float bottom, float top, float n, float f ) {
+void Set2DWindow(int x, int y, int w, int h, float left, float right, float bottom, float top, float n, float f) {
 	R_SyncRenderThread();
 	qglViewport(x, y, w, h);
 	qglScissor(x, y, w, h);
@@ -395,7 +399,7 @@ void Set2DWindow( int x, int y, int w, int h, float left, float right, float bot
 RE_Scissor
 ================
 */
-void RE_Scissor( int x, int y, int width, int height ) {
-	qglEnable( GL_SCISSOR_TEST );
-	qglScissor( x, y, width, height );
+void RE_Scissor(int x, int y, int width, int height) {
+	qglEnable(GL_SCISSOR_TEST);
+	qglScissor(x, y, width, height);
 }
