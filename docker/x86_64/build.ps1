ceea7cb192d5774ce6e9c4765c8f1de0f29f5163
OM <private@private.com>
2023-06-18 03:10:06 +0200

    Added i386

--- docker/x86_64/build.ps1
+++ docker/x86_64/build.ps1
@@ -1,6 +1,7 @@
 docker build -t openmohaa/builder.14_04 14.04 && `
 docker build -t openmohaa/builder.16_04 16.04 && `
 docker build -t openmohaa/builder.18_04 18.04 && `
+docker build -t openmohaa/builder.18_04 18.04-i386 && `
 docker build -t openmohaa/builder.20_04 20.04 && `
 docker build -t openmohaa/builder.22_04 22.04 && `
 docker build -t openmohaa/builder.22_10 22.10
