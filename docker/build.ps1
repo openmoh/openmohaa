de0a68ef9068ae19c5dbdbeadec064ec5343b4af
OM <private@private.com>
2023-06-18 03:13:56 +0200

    Moved 18.04-i386 to i386 folder

---
+++ docker/build.ps1
@@ -0,0 +1,7 @@
+docker build -t openmohaa/builder.14_04 x86_64/14.04 && `
+docker build -t openmohaa/builder.16_04 x86_64/16.04 && `
+docker build -t openmohaa/builder.18_04 x86_64/18.04 && `
+docker build -t openmohaa/builder.20_04 x86_64/20.04 && `
+docker build -t openmohaa/builder.22_04 x86_64/22.04 && `
+docker build -t openmohaa/builder.22_10 x86_64/22.10 && `
+docker build -t openmohaa/builder.18_04.i386 i386/18.04
