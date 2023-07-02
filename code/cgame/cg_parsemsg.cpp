7db4d1a3f41ea0c191b6e3cd4f9d5d386c08bb0e
OM <private@private.com>
2023-07-02 19:18:37 +0200

    Newline fix

--- code/cgame/cg_parsemsg.cpp
+++ code/cgame/cg_parsemsg.cpp
@@ -1147,7 +1147,8 @@ void CG_ParseCGMessage_ver_6()
 }
 
 void CG_InitCGMessageAPI(clientGameExport_t* cge)
-{	if (cg_protocol >= PROTOCOL_MOHTA_MIN) {
+{
+    if (cg_protocol >= PROTOCOL_MOHTA_MIN) {
 		cge->CG_ParseCGMessage = &CG_ParseCGMessage_ver_15;
     } else {
 		cge->CG_ParseCGMessage = &CG_ParseCGMessage_ver_6;
