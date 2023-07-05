13828994fbe07a28188d6b020752b0c5dcd2c601
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-07-05 21:24:14 +0200

    Used clang-format on gamespy files

--- code/gamespy/sv_gamespy.c
+++ code/gamespy/sv_gamespy.c
@@ -26,29 +26,18 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "gcdkey/gcdkeys.h"
 
-static char gamemode[128];
+static char     gamemode[128];
 static qboolean gcdInitialized = qfalse;
 
-static const char *SECRET_GS_KEYS[] =
-{
-	"M5Fdwc",
-	"h2P1c9",
-	"y32FDc"
-};
+static const char *SECRET_GS_KEYS[] = {"M5Fdwc", "h2P1c9", "y32FDc"};
 
-static const unsigned int GCD_GAME_IDS[] =
-{
+static const unsigned int GCD_GAME_IDS[] = {
     0,
-	641,
-	802,
+    641,
+    802,
 };
 
-static const char* GS_GAME_NAME[] =
-{
-    "mohaa",
-    "mohaas",
-    "mohaab"
-};
+static const char *GS_GAME_NAME[] = {"mohaa", "mohaas", "mohaab"};
 
 static const unsigned int GAMESPY_DEFAULT_PORT = 12300;
 
@@ -57,35 +46,34 @@ void qr_shutdown(qr_t qrec);
 void qr_process_queries(qr_t qrec);
 
 int qr_init(
-	qr_t* qrec,
-	char* ip,
-	int baseport,
-	char* gamename,
-	char* secret_key,
-	qr_querycallback_t qr_basic_callback,
-	qr_querycallback_t qr_info_callback,
-	qr_querycallback_t qr_rules_callback,
-	qr_querycallback_t qr_players_callback,
-	void* userdata
+    qr_t              *qrec,
+    char              *ip,
+    int                baseport,
+    char              *gamename,
+    char              *secret_key,
+    qr_querycallback_t qr_basic_callback,
+    qr_querycallback_t qr_info_callback,
+    qr_querycallback_t qr_rules_callback,
+    qr_querycallback_t qr_players_callback,
+    void              *userdata
 );
 
-static const char* ConvertMapFilename(const char* mapname)
+static const char *ConvertMapFilename(const char *mapname)
 {
-	static char converted[1024];
+    static char converted[1024];
 
-	const char* name = strstr(mapname, "/");
-	if (!name) {
-		return mapname;
-	}
+    const char *name = strstr(mapname, "/");
+    if (!name) {
+        return mapname;
+    }
 
-	strcpy(converted, name + 1);
-	return converted;
+    strcpy(converted, name + 1);
+    return converted;
 }
 
-static void basic_callback(char* outbuf, int maxlen, void* userdata)
+static void basic_callback(char *outbuf, int maxlen, void *userdata)
 {
-	switch (com_target_game->integer)
-	{
+    switch (com_target_game->integer) {
     case TG_MOH:
         Info_SetValueForKey(outbuf, "gamename", TARGET_GAME_NAME_MOH);
         Info_SetValueForKey(outbuf, "gamever", TARGET_GAME_VERSION_MOH);
@@ -98,203 +86,198 @@ static void basic_callback(char* outbuf, int maxlen, void* userdata)
         Info_SetValueForKey(outbuf, "gamename", TARGET_GAME_NAME_MOH);
         Info_SetValueForKey(outbuf, "gamever", TARGET_GAME_VERSION_MOHTT);
         break;
-	}
+    }
 
-	Info_SetValueForKey(outbuf, "location", va("%i", sv_location->integer));
+    Info_SetValueForKey(outbuf, "location", va("%i", sv_location->integer));
 
-	if (sv_debug_gamespy->integer) {
-		Com_DPrintf("Basic callback, sent: %s\n\n", outbuf);
-	}
+    if (sv_debug_gamespy->integer) {
+        Com_DPrintf("Basic callback, sent: %s\n\n", outbuf);
+    }
 }
 
-static void info_callback(char* outbuf, int maxlen, void* userdata)
+static void info_callback(char *outbuf, int maxlen, void *userdata)
 {
-	char infostring[1024];
-	qboolean allowlean = qfalse;
+    char     infostring[1024];
+    qboolean allowlean = qfalse;
 
-	infostring[0] = 0;
-	Info_SetValueForKey(infostring, "hostname", sv_hostname->string);
-	Info_SetValueForKey(infostring, "hostport", Cvar_Get("net_port", "12203", 32)->string);
-	Info_SetValueForKey(infostring, "mapname", ConvertMapFilename(svs.mapName));
-	Info_SetValueForKey(infostring, "gametype", g_gametypestring->string);
-	Info_SetValueForKey(infostring, "numplayers", va("%i", SV_NumClients()));
-	Info_SetValueForKey(infostring, "maxplayers", va("%i", svs.iNumClients - sv_privateClients->integer));
-	Info_SetValueForKey(infostring, "gamemode", gamemode);
-	Info_SetValueForKey(infostring, "gametype_i", va("%i", g_gametype->integer));
-	Info_SetValueForKey(infostring, "dedicated", Cvar_Get("ui_dedicated", "0", 0)->string);
-	Info_SetValueForKey(infostring, "sprinton", Cvar_Get("sv_sprinton", "1", 0)->string);
-	Info_SetValueForKey(infostring, "realism", Cvar_Get("g_realismmode", "0", 0)->string);
-	Info_SetValueForKey(infostring, "pure", va("%i", sv_pure->integer));
-	if ((Cvar_VariableIntegerValue("dmflags") & 0x40000) != 0) {
-		allowlean = 1;
-	}
+    infostring[0] = 0;
+    Info_SetValueForKey(infostring, "hostname", sv_hostname->string);
+    Info_SetValueForKey(infostring, "hostport", Cvar_Get("net_port", "12203", 32)->string);
+    Info_SetValueForKey(infostring, "mapname", ConvertMapFilename(svs.mapName));
+    Info_SetValueForKey(infostring, "gametype", g_gametypestring->string);
+    Info_SetValueForKey(infostring, "numplayers", va("%i", SV_NumClients()));
+    Info_SetValueForKey(infostring, "maxplayers", va("%i", svs.iNumClients - sv_privateClients->integer));
+    Info_SetValueForKey(infostring, "gamemode", gamemode);
+    Info_SetValueForKey(infostring, "gametype_i", va("%i", g_gametype->integer));
+    Info_SetValueForKey(infostring, "dedicated", Cvar_Get("ui_dedicated", "0", 0)->string);
+    Info_SetValueForKey(infostring, "sprinton", Cvar_Get("sv_sprinton", "1", 0)->string);
+    Info_SetValueForKey(infostring, "realism", Cvar_Get("g_realismmode", "0", 0)->string);
+    Info_SetValueForKey(infostring, "pure", va("%i", sv_pure->integer));
+    if ((Cvar_VariableIntegerValue("dmflags") & 0x40000) != 0) {
+        allowlean = 1;
+    }
 
-	Info_SetValueForKey(infostring, "allowlean", va("%i", allowlean));
-	if (strlen(infostring) < maxlen) {
-		strcpy(outbuf, infostring);
-	}
+    Info_SetValueForKey(infostring, "allowlean", va("%i", allowlean));
+    if (strlen(infostring) < maxlen) {
+        strcpy(outbuf, infostring);
+    }
 
-	if (sv_debug_gamespy->integer) {
-		Com_DPrintf("Info callback, sent: %s\n\n", outbuf);
-	}
+    if (sv_debug_gamespy->integer) {
+        Com_DPrintf("Info callback, sent: %s\n\n", outbuf);
+    }
 }
 
-static void rules_callback(char* outbuf, int maxlen, void* userdata)
+static void rules_callback(char *outbuf, int maxlen, void *userdata)
 {
-	char infostring[1024];
+    char infostring[1024];
 
-	infostring[0] = 0;
+    infostring[0] = 0;
 
-	Info_SetValueForKey(infostring, "timelimit", Cvar_VariableString("timelimit"));
-	Info_SetValueForKey(infostring, "fraglimit", Cvar_VariableString("fraglimit"));
-	Info_SetValueForKey(infostring, "rankedserver", Cvar_VariableString("g_rankedserver"));
+    Info_SetValueForKey(infostring, "timelimit", Cvar_VariableString("timelimit"));
+    Info_SetValueForKey(infostring, "fraglimit", Cvar_VariableString("fraglimit"));
+    Info_SetValueForKey(infostring, "rankedserver", Cvar_VariableString("g_rankedserver"));
 
-	if (strlen(infostring) < maxlen) {
-		strcpy(outbuf, infostring);
-	}
+    if (strlen(infostring) < maxlen) {
+        strcpy(outbuf, infostring);
+    }
 
-	if (sv_debug_gamespy->integer) {
-		Com_DPrintf("Rules callback, sent: %s\n\n", outbuf);
-	}
+    if (sv_debug_gamespy->integer) {
+        Com_DPrintf("Rules callback, sent: %s\n\n", outbuf);
+    }
 }
 
-static void players_callback(char* outbuf, int maxlen, void* userdata)
+static void players_callback(char *outbuf, int maxlen, void *userdata)
 {
-	int i;
-	char infostring[128];
-	size_t infolen;
-	size_t currlen = 0;
+    int    i;
+    char   infostring[128];
+    size_t infolen;
+    size_t currlen = 0;
 
-	for (i = 0; i < svs.iNumClients; i++)
-	{
-		playerState_t* ps = SV_GameClientNum(i);
+    for (i = 0; i < svs.iNumClients; i++) {
+        playerState_t *ps = SV_GameClientNum(i);
 
-		Com_sprintf(
-			infostring,
-			128,
-			"\\player_%d\\%s\\frags_%d\\%d\\deaths_%d\\%d\\ping_%d\\%d",
-			i,
-			svs.clients[i].name,
-			i,
-			ps->stats[STAT_KILLS],
-			i,
-			ps->stats[STAT_DEATHS],
-			i,
-			svs.clients[i].ping
-		);
+        Com_sprintf(
+            infostring,
+            128,
+            "\\player_%d\\%s\\frags_%d\\%d\\deaths_%d\\%d\\ping_%d\\%d",
+            i,
+            svs.clients[i].name,
+            i,
+            ps->stats[STAT_KILLS],
+            i,
+            ps->stats[STAT_DEATHS],
+            i,
+            svs.clients[i].ping
+        );
 
-		infolen = strlen(infostring);
-		if (currlen + infolen < maxlen)
-		{
-			strcat(outbuf, infostring);
-			currlen += infolen;
-		}
-	}
+        infolen = strlen(infostring);
+        if (currlen + infolen < maxlen) {
+            strcat(outbuf, infostring);
+            currlen += infolen;
+        }
+    }
 }
 
 void SV_GamespyHeartbeat()
 {
-	if (!g_gametype->integer || !sv_gamespy->integer) {
-		return;
-	}
+    if (!g_gametype->integer || !sv_gamespy->integer) {
+        return;
+    }
 
-	if (sv_debug_gamespy->integer) {
-		Com_DPrintf("GameSpy Heartbeat\n");
-	}
+    if (sv_debug_gamespy->integer) {
+        Com_DPrintf("GameSpy Heartbeat\n");
+    }
 
-	qr_send_statechanged(NULL);
+    qr_send_statechanged(NULL);
 }
 
 void SV_ProcessGamespyQueries()
 {
-	if (!g_gametype->integer || !sv_gamespy->integer) {
-		return;
-	}
+    if (!g_gametype->integer || !sv_gamespy->integer) {
+        return;
+    }
 
-	qr_process_queries(NULL);
-	gcd_think();
+    qr_process_queries(NULL);
+    gcd_think();
 }
 
 void SV_ShutdownGamespy()
 {
-	if (!g_gametype->integer || !sv_gamespy->integer) {
-		return;
-	}
+    if (!g_gametype->integer || !sv_gamespy->integer) {
+        return;
+    }
 
-	strcpy(gamemode, "exiting");
+    strcpy(gamemode, "exiting");
 
-	if (gcdInitialized)
-	{
-		gcd_shutdown();
-		gcdInitialized = 0;
-	}
+    if (gcdInitialized) {
+        gcd_shutdown();
+        gcdInitialized = 0;
+    }
 
-	qr_send_statechanged(NULL);
-	qr_shutdown(NULL);
+    qr_send_statechanged(NULL);
+    qr_shutdown(NULL);
 }
 
 qboolean SV_InitGamespy()
 {
-	cvar_t* net_ip;
-	cvar_t* net_gamespy_port;
-	char secret_key[9];
-	const char* secret_gs_key;
-	const char* gs_game_name;
-	int gcd_game_id;
+    cvar_t     *net_ip;
+    cvar_t     *net_gamespy_port;
+    char        secret_key[9];
+    const char *secret_gs_key;
+    const char *gs_game_name;
+    int         gcd_game_id;
 
     if (com_target_game->integer > ARRAY_LEN(SECRET_GS_KEYS)) {
-		Com_Error(ERR_DROP, "Invalid target game %d for GameSpy", com_target_game->integer);
-		return qfalse;
+        Com_Error(ERR_DROP, "Invalid target game %d for GameSpy", com_target_game->integer);
+        return qfalse;
     }
 
-	secret_gs_key = SECRET_GS_KEYS[com_target_game->integer];
-	gcd_game_id = GCD_GAME_IDS[com_target_game->integer];
-	gs_game_name = GS_GAME_NAME[com_target_game->integer];
+    secret_gs_key = SECRET_GS_KEYS[com_target_game->integer];
+    gcd_game_id   = GCD_GAME_IDS[com_target_game->integer];
+    gs_game_name  = GS_GAME_NAME[com_target_game->integer];
 
-	sv_debug_gamespy = Cvar_Get("sv_debuggamespy", "0", 0);
-	sv_location = Cvar_Get("sv_location", "1", CVAR_ARCHIVE);
-	sv_gamespy = Cvar_Get("sv_gamespy", "1", CVAR_LATCH);
+    sv_debug_gamespy = Cvar_Get("sv_debuggamespy", "0", 0);
+    sv_location      = Cvar_Get("sv_location", "1", CVAR_ARCHIVE);
+    sv_gamespy       = Cvar_Get("sv_gamespy", "1", CVAR_LATCH);
 
-	if (!sv_gamespy->integer || !g_gametype->integer) {
-		return qfalse;
-	}
+    if (!sv_gamespy->integer || !g_gametype->integer) {
+        return qfalse;
+    }
 
-	strcpy(gamemode, "openplaying");
-	strcpy(secret_key, secret_gs_key);
+    strcpy(gamemode, "openplaying");
+    strcpy(secret_key, secret_gs_key);
 
-	net_ip = Cvar_Get("net_ip", "localhost", CVAR_LATCH);
-	net_gamespy_port = Cvar_Get("net_gamespy_port", va("%i", GAMESPY_DEFAULT_PORT), CVAR_LATCH);
+    net_ip           = Cvar_Get("net_ip", "localhost", CVAR_LATCH);
+    net_gamespy_port = Cvar_Get("net_gamespy_port", va("%i", GAMESPY_DEFAULT_PORT), CVAR_LATCH);
 
-	if (qr_init(
-		NULL,
-		net_ip->string,
-		net_gamespy_port->integer,
-		gs_game_name,
-		secret_key,
-		basic_callback,
-		info_callback,
-		rules_callback,
-		players_callback,
-		NULL))
-	{
-		Com_DPrintf("Error starting query sockets in SV_GamespyInit\n");
-		return qfalse;
-	}
+    if (qr_init(
+            NULL,
+            net_ip->string,
+            net_gamespy_port->integer,
+            gs_game_name,
+            secret_key,
+            basic_callback,
+            info_callback,
+            rules_callback,
+            players_callback,
+            NULL
+        )) {
+        Com_DPrintf("Error starting query sockets in SV_GamespyInit\n");
+        return qfalse;
+    }
 
-	if (!sv_gamespy->integer)
-	{
-		strcpy(gamemode, "exiting");
-		qr_send_statechanged(NULL);
-	}
+    if (!sv_gamespy->integer) {
+        strcpy(gamemode, "exiting");
+        qr_send_statechanged(NULL);
+    }
 
-	if (!gcdInitialized)
-	{
-		if (gcd_game_id) {
-			gcd_init(gcd_game_id);
-		}
+    if (!gcdInitialized) {
+        if (gcd_game_id) {
+            gcd_init(gcd_game_id);
+        }
 
-		gcdInitialized = 1;
-	}
+        gcdInitialized = 1;
+    }
 
-	return qtrue;
+    return qtrue;
 }
--- code/gamespy/sv_gqueryreporting.h
+++ code/gamespy/sv_gqueryreporting.h
@@ -24,39 +24,39 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "common/gsPlatformSocket.h"
 
-typedef void (*qr_querycallback_t) (char* outbuf, int maxlen, void* userdata);
-typedef void (*qr_custom_handler_t) (const char* query, struct sockaddr* sender);
+typedef void (*qr_querycallback_t)(char *outbuf, int maxlen, void *userdata);
+typedef void (*qr_custom_handler_t)(const char *query, struct sockaddr *sender);
 
 typedef enum query_e {
-	qtunknown,
-	qtbasic,
-	qtinfo,
-	qtrules,
-	qtplayers,
-	qtstatus,
-	qtpackets,
-	qtecho,
-	qtsecure
+    qtunknown,
+    qtbasic,
+    qtinfo,
+    qtrules,
+    qtplayers,
+    qtstatus,
+    qtpackets,
+    qtecho,
+    qtsecure
 } query_t;
 
 typedef struct qr_implementation_s {
-	void* querysock;
-	void* hbsock;
-	char gamename[64];
-	char secret_key[128];
-	qr_querycallback_t qr_basic_callback;
-	qr_querycallback_t qr_info_callback;
-	qr_querycallback_t qr_rules_callback;
-	qr_querycallback_t qr_players_callback;
-	long unsigned int lastheartbeat;
-	int queryid;
-	int packetnumber;
-	int qport;
-	char no_query;
-	struct sockaddr_in master_saddr;
-	int unk2;
-	int unk3;
-	int unk4;
-	qr_custom_handler_t qr_custom_handler;
-	void* udata;
+    void               *querysock;
+    void               *hbsock;
+    char                gamename[64];
+    char                secret_key[128];
+    qr_querycallback_t  qr_basic_callback;
+    qr_querycallback_t  qr_info_callback;
+    qr_querycallback_t  qr_rules_callback;
+    qr_querycallback_t  qr_players_callback;
+    long unsigned int   lastheartbeat;
+    int                 queryid;
+    int                 packetnumber;
+    int                 qport;
+    char                no_query;
+    struct sockaddr_in  master_saddr;
+    int                 unk2;
+    int                 unk3;
+    int                 unk4;
+    qr_custom_handler_t qr_custom_handler;
+    void               *udata;
 } qr_implementation_t, *qr_t;
