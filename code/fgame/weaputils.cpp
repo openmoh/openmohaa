b7fac3a5b0c5dcbf96600d031f1b21ba22004981
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-08-02 21:58:47 +0200

    Added BulletAttack_Stat

--- code/fgame/weaputils.cpp
+++ code/fgame/weaputils.cpp
@@ -2136,6 +2136,104 @@ Projectile *ProjectileAttack
 	return proj;
 }
 
+void BulletAttack_Stat(Entity* owner, Entity* target, trace_t* trace, Weapon* weap) {
+	Sentient* targetSen;
+	
+	if (!target->IsSubclassOfSentient() || !weap) {
+		return;
+	}
+
+	targetSen = static_cast<Sentient*>(target);
+	targetSen->m_iLastHitTime = level.inttime;
+	weap->m_iNumHits++;
+
+	switch (trace->location)
+	{
+	case LOCATION_HEAD:
+	case LOCATION_HELMET:
+	case LOCATION_NECK:
+		weap->m_iNumHeadShots++;
+		break;
+	case LOCATION_TORSO_UPPER:
+	case LOCATION_TORSO_MID:
+	case LOCATION_TORSO_LOWER:
+		weap->m_iNumTorsoShots++;
+		break;
+	case LOCATION_PELVIS:
+		weap->m_iNumGroinShots++;
+		break;
+	case LOCATION_R_ARM_UPPER:
+	case LOCATION_R_ARM_LOWER:
+	case LOCATION_R_HAND:
+		weap->m_iNumRightArmShots++;
+        break;
+    case LOCATION_L_ARM_UPPER:
+    case LOCATION_L_ARM_LOWER:
+    case LOCATION_L_HAND:
+        weap->m_iNumLeftArmShots++;
+        break;
+	case LOCATION_R_LEG_UPPER:
+	case LOCATION_R_LEG_LOWER:
+	case LOCATION_R_FOOT:
+		weap->m_iNumRightLegShots++;
+        break;
+    case LOCATION_L_LEG_UPPER:
+    case LOCATION_L_LEG_LOWER:
+    case LOCATION_L_FOOT:
+        weap->m_iNumLeftLegShots++;
+        break;
+	default:
+		weap->m_iNumTorsoShots++;
+		break;
+	}
+
+	if (owner && owner->IsSubclassOfPlayer() && weap->IsSubclassOfTurretGun()) {
+		Player* p = static_cast<Player*>(owner);
+
+		p->m_iNumHits++;
+
+        switch (trace->location)
+        {
+        case LOCATION_HEAD:
+        case LOCATION_HELMET:
+        case LOCATION_NECK:
+            p->m_iNumHeadShots++;
+            break;
+        case LOCATION_TORSO_UPPER:
+        case LOCATION_TORSO_MID:
+        case LOCATION_TORSO_LOWER:
+            p->m_iNumTorsoShots++;
+            break;
+        case LOCATION_PELVIS:
+            p->m_iNumGroinShots++;
+            break;
+        case LOCATION_R_ARM_UPPER:
+        case LOCATION_R_ARM_LOWER:
+        case LOCATION_R_HAND:
+            p->m_iNumRightArmShots++;
+            break;
+        case LOCATION_L_ARM_UPPER:
+        case LOCATION_L_ARM_LOWER:
+        case LOCATION_L_HAND:
+            p->m_iNumLeftArmShots++;
+            break;
+        case LOCATION_R_LEG_UPPER:
+        case LOCATION_R_LEG_LOWER:
+        case LOCATION_R_FOOT:
+            p->m_iNumRightLegShots++;
+            break;
+        case LOCATION_L_LEG_UPPER:
+        case LOCATION_L_LEG_LOWER:
+        case LOCATION_L_FOOT:
+            p->m_iNumLeftLegShots++;
+            break;
+        default:
+            p->m_iNumTorsoShots++;
+            break;
+        }
+	}
+}
+
 float BulletAttack
 	(
 	Vector   start,
