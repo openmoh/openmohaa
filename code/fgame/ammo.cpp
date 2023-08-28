65dcb72cbac209cd181a252051a4b53654760566
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-08-28 15:41:06 +0200

    Fixed AmmoPickup not sending a message to the player

--- code/fgame/ammo.cpp
+++ code/fgame/ammo.cpp
@@ -21,163 +21,147 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 // ammo.cpp: Base class for all ammunition for entities derived from the Weapon class.
-// 
+//
 // AmmoEntity is the Class which represents ammo that the player "sees" and "
 // picks up" in the game
 //
-// Ammo is the Class which is used to keep track of how much ammo a player has 
+// Ammo is the Class which is used to keep track of how much ammo a player has
 // in his inventory
 
 #include "ammo.h"
 #include "player.h"
 
-CLASS_DECLARATION( Item, AmmoEntity, NULL )
-	{
-		{ NULL, NULL }
-	};
+CLASS_DECLARATION(Item, AmmoEntity, NULL) {
+    {NULL, NULL}
+};
 
-AmmoEntity::AmmoEntity
-   (
-   )
+AmmoEntity::AmmoEntity()
 
-	{
-   if ( LoadingSavegame )
-      {
-      // all data will be setup by the archive function
-      return;
-      }
-   setName( "UnknownAmmo" );
-   amount       = 0;
-	}
+{
+    if (LoadingSavegame) {
+        // all data will be setup by the archive function
+        return;
+    }
+    setName("UnknownAmmo");
+    amount = 0;
+}
 
-Item *AmmoEntity::ItemPickup
-   (
-   Entity *other, 
-	qboolean add_to_inventory
-   )
+Item *AmmoEntity::ItemPickup(Entity *other, qboolean add_to_inventory)
 
-   {
-   Sentient *player;
-	str      realname;
-	AliasListNode_t *ret;
+{
+    Sentient *player;
+    Weapon* pWeap;
 
-   if ( !other->isSubclassOf( Player ) )
-      return NULL;
+    if (!other->IsSubclassOfPlayer()) {
+        return NULL;
+    }
 
-   player = ( Sentient * )other;
+    player = (Sentient *)other;
 
-	// Play pickup sound
-   realname = GetRandomAlias( "snd_pickup", &ret );
-   if ( realname.length() > 1 )
-		player->Sound( realname, CHAN_ITEM );
+    if (player->AmmoCount(item_name.c_str()) == player->MaxAmmoCount(item_name.c_str())) {
+        // don't pickup if the player already has max ammo
+        return NULL;
+    }
 
-   // Cancel some events
-	CancelEventsOfType( EV_Item_DropToFloor );
-	CancelEventsOfType( EV_Item_Respawn );
-   CancelEventsOfType( EV_FadeOut );
+    // Play pickup sound
+    player->Sound(sPickupSound, CHAN_ITEM);
 
-   // Hide the model
-	setSolidType( SOLID_NOT );
-	hideModel();
+    // Cancel some events
+    CancelEventsOfType(EV_Item_DropToFloor);
+    CancelEventsOfType(EV_Item_Respawn);
+    CancelEventsOfType(EV_FadeOut);
 
-   // Respawn?
-	if ( !Respawnable() )
-		PostEvent( EV_Remove, FRAMETIME );
-   else 
-		PostEvent( EV_Item_Respawn, RespawnTime() );
+    // Hide the model
+    setSolidType(SOLID_NOT);
+    hideModel();
 
-   // Give the ammo to the player
-   player->GiveAmmo( item_name, amount );
-   return NULL; // This doesn't create any items
-   }
+    // Respawn?
+    if (!Respawnable()) {
+        PostEvent(EV_Remove, FRAMETIME);
+    } else {
+        PostEvent(EV_Item_Respawn, RespawnTime());
+    }
 
+    // Notify scripts about the pickup
+    Unregister(STRING_PICKUP);
+
+    // Tell the player about the ammo being picked
+    gi.SendServerCommand(other->edict - g_entities, "print \"" HUD_MESSAGE_YELLOW "%s\"", gi.LV_ConvertString(va("Got %d %s Rounds", amount, item_name.c_str())));
+
+    // Give the ammo to the player
+    player->GiveAmmo(item_name, amount);
+
+    // Make the weapon reload if the weapon has an empty clip
+    pWeap = player->GetActiveWeapon(WEAPON_MAIN);
+    if (pWeap && !pWeap->HasAmmoInClip(FIRE_PRIMARY) && pWeap->CheckReload(FIRE_PRIMARY)) {
+        pWeap->SetShouldReload(qtrue);
+    }
+
+    return NULL; // This doesn't create any items
+}
 
 // This is the Class that is used to keep track of ammo in the player's inventory.
 // It is not an entit, just a name and an amount.
 
-CLASS_DECLARATION( Class, Ammo, NULL )
-   {
-      {NULL, NULL}
-   };
+CLASS_DECLARATION(Class, Ammo, NULL) {
+    {NULL, NULL}
+};
 
-Ammo::Ammo
-   (
-   )
+Ammo::Ammo()
 
-   {
-   if ( LoadingSavegame )
-      {
-      // all data will be setup by the archive function
-      return;
-      }
-   setName( "UnknownAmmo" );
-   setAmount( 0 );
-   setMaxAmount( 100 );
-   }
+{
+    if (LoadingSavegame) {
+        // all data will be setup by the archive function
+        return;
+    }
+    setName("UnknownAmmo");
+    setAmount(0);
+    setMaxAmount(100);
+}
 
-void Ammo::setAmount
-   (
-   int a
-   )
+void Ammo::setAmount(int a)
 
-   {
-   amount = a;
+{
+    amount = a;
 
-   if ( ( maxamount > 0 ) && ( amount > maxamount ) )
-      amount = maxamount;
-   }
+    if ((maxamount > 0) && (amount > maxamount)) {
+        amount = maxamount;
+    }
+}
 
-int Ammo::getAmount
-   (
-   void
-   )
+int Ammo::getAmount(void)
 
-   {
-   return amount;
-   }
+{
+    return amount;
+}
 
-void Ammo::setMaxAmount
-   (
-   int a
-   )
+void Ammo::setMaxAmount(int a)
 
-   {
-   maxamount = a;
-   }
+{
+    maxamount = a;
+}
 
-int Ammo::getMaxAmount
-   (
-   void
-   )
+int Ammo::getMaxAmount(void)
 
-   {
-   return maxamount;
-   }
+{
+    return maxamount;
+}
 
-void Ammo::setName
-   (
-   str n
-   )
+void Ammo::setName(str n)
 
-   {
-   name = n;
-   name_index = gi.itemindex( name ) + CS_WEAPONS;
-   }
+{
+    name       = n;
+    name_index = gi.itemindex(name) + CS_WEAPONS;
+}
 
-str Ammo::getName
-   (
-   void
-   )
+str Ammo::getName(void)
 
-   {
-   return name;
-   }
+{
+    return name;
+}
 
-int Ammo::getIndex
-   (
-   void
-   )
+int Ammo::getIndex(void)
 
-   {
-   return name_index;
-   }
+{
+    return name_index;
+}
