07e8c6feaaf3323bcab3a5513b51e054a0a02873
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-10-01 13:42:16 +0200

    Fully implemented cMoveGrid

--- code/fgame/movegrid.cpp
+++ code/fgame/movegrid.cpp
@@ -24,200 +24,214 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
 #include "movegrid.h"
+#include "debuglines.h"
 
-// FIXME: undone class
-
-cMoveGrid::cMoveGrid( int x, int y, int z )
+cMoveGrid::cMoveGrid(int x, int y, int z)
 {
-	m_iXRes = x;
-	m_iYRes = y;
-	m_iZRes = z;
+    m_iXRes = x;
+    m_iYRes = y;
+    m_iZRes = z;
 
-	GridPoints = ( gridpoint_t * )gi.Malloc( sizeof( gridpoint_t ) * x * y * z );
+    GridPoints = (gridpoint_t *)gi.Malloc(sizeof(gridpoint_t) * x * y * z);
 }
 
 cMoveGrid::~cMoveGrid()
 {
-	gi.Free( GridPoints );
+    gi.Free(GridPoints);
 }
 
-void cMoveGrid::SetOrientation
-	(
-	float( *v )[ 3 ]
-	)
+void cMoveGrid::SetMoveInfo(vmove_t *vm)
 {
-	for( int i = 0; i < 3; i++ )
-	{
-		VectorCopy( v[ i ], orientation[ i ] );
-	}
+    v = *vm;
 }
 
-void cMoveGrid::SetMoveInfo
-	(
-	vmove_t *vm
-	)
+void cMoveGrid::GetMoveInfo(vmove_t *vm)
 {
-	memcpy( &v, vm, sizeof( vmove_t ) );
+    *vm = v;
 }
 
-void cMoveGrid::CalculateBoxPoints
-	(
-	void
-	)
+gridpoint_t *cMoveGrid::GetGridPoint(int x, int y, int z)
 {
-	int x, y, z;
-	int rx, ry, rz;
-	int xdelta, ydelta, zdelta;
-
-	Vector vBoxSize = Vector(v.maxs) - Vector(v.mins);
-
-	rx = (int)(vBoxSize[0] / (float)m_iXRes);
-	ry = (int)(vBoxSize[1] / (float)m_iYRes);
-	rz = (int)(vBoxSize[2] / (float)m_iZRes);
-
-	xdelta = ydelta = zdelta = 0;
-
-	for (x = 0; x < m_iXRes; x++)
-	{
-		for (y = 0; y < m_iYRes; y++)
-		{
-			for (z = 0; z < m_iZRes; z++)
-			{
-				gridpoint_t* gridPoint = GetGridPoint(x, y, z);
-
-				gridPoint->origin = Vector(
-					(float)(xdelta - rx),
-					(float)(ydelta - ry),
-					(float)rz * 0.5f
-				);
-			}
-		}
-	}
-
-	for (x = 0; x < m_iXRes; x++)
-	{
-		for (y = 0; y < m_iYRes; y++)
-		{
-			for (z = 0; z < m_iZRes; z++)
-			{
-				gridpoint_t* gridPoint = GetGridPoint(x, y, z);
-
-				Vector vTmp;
-				MatrixTransformVector(gridPoint->origin, orientation, vTmp);
-
-				gridPoint->origin = vTmp;
-				memcpy(&gridPoint->vm, &v, sizeof(gridPoint->vm));
-				gridPoint->vm.vs = &gridPoint->vs;
-
-				if (rx >= ry)
-				{
-					VectorSet(
-						gridPoint->vm.mins,
-						(float)-ry * 0.5f,
-						(float)-ry * 0.5f,
-						(float)-rz * 0.5f
-					);
-
-					VectorSet(
-						gridPoint->vm.maxs,
-						(float)ry * 0.5f,
-						(float)ry * 0.5f,
-						(float)rz * 0.5f
-					);
-				}
-				else
-				{
-					VectorSet(
-						gridPoint->vm.mins,
-						(float)-rx * 0.5f,
-						(float)-rx * 0.5f,
-						(float)-rz * 0.5f
-					);
-
-					VectorSet(
-						gridPoint->vm.maxs,
-						(float)rx * 0.5f,
-						(float)rx * 0.5f,
-						(float)rz * 0.5f
-					);
-				}
-			}
-		}
-	}
+    return &GridPoints[x * m_iZRes + y * m_iXRes + z];
 }
 
-gridpoint_t *cMoveGrid::GetGridPoint
-	(
-	int x,
-	int y,
-	int z
-	)
+void cMoveGrid::SetOrientation(const vec3_t *v)
 {
-	return &GridPoints[ x * m_iZRes + y * m_iXRes + z ];
+    for (int i = 0; i < 3; i++) {
+        VectorCopy(v[i], orientation[i]);
+    }
 }
 
-void cMoveGrid::Move
-	(
-	void
-	)
+void cMoveGrid::CalculateBoxPoints(void)
 {
-	int x;
-	int y;
-	int z;
-	Vector vBoxSize;
-	Vector old_origin;
+    int    x, y, z;
+    int    rx, ry, rz;
+    int    xdelta, ydelta, zdelta;
+    Vector vBoxSize;
 
-	CalculateBoxPoints();
+    vBoxSize = Vector(v.maxs) - Vector(v.mins);
 
-	for( x = 0; x < m_iXRes; x++ )
-	{
-		for( y = 0; y < m_iYRes; y++ )
-		{
-			for( z = 0; z < m_iZRes; z++ )
-			{
-			}
-		}
-	}
+    rx = (int)(vBoxSize.x / (float)m_iXRes);
+    ry = (int)(vBoxSize.y / (float)m_iYRes);
+    rz = (int)(vBoxSize.z / (float)m_iZRes);
 
-	// this is not called anyways
-	// FIXME: stub
-	STUB();
+    xdelta = ydelta = zdelta = 0;
+
+    for (x = 0; x < m_iXRes; x++) {
+        for (y = 0; y < m_iYRes; y++) {
+            for (z = 0; z < m_iZRes; z++) {
+                gridpoint_t *gridPoint = GetGridPoint(x, y, z);
+
+                gridPoint->origin = Vector(xdelta - rx, ydelta - ry, zdelta + rz / 2);
+                zdelta += rz;
+            }
+            zdelta = 0;
+            ydelta += ry;
+        }
+        ydelta = 0;
+        xdelta += rx;
+    }
+
+    for (x = 0; x < m_iXRes; x++) {
+        for (y = 0; y < m_iYRes; y++) {
+            for (z = 0; z < m_iZRes; z++) {
+                Vector       vTmp;
+                gridpoint_t *gridPoint = GetGridPoint(x, y, z);
+
+                MatrixTransformVector(gridPoint->origin, orientation, vTmp);
+
+                gridPoint->origin = vTmp;
+                gridPoint->vm     = v;
+                gridPoint->vm.vs  = &gridPoint->vs;
+
+                if (rx >= ry) {
+                    Vector vec;
+
+                    vec = Vector(-ry / 2, -ry / 2, -rz / 2);
+                    vec.copyTo(gridPoint->vm.mins);
+
+                    vec = Vector(ry / 2, ry / 2, rz / 2);
+                    vec.copyTo(gridPoint->vm.maxs);
+                } else {
+                    Vector vec;
+
+                    vec = Vector(-rx / 2, -rx / 2, -rz / 2);
+                    vec.copyTo(gridPoint->vm.mins);
+
+                    vec = Vector(rx / 2, rx / 2, rz / 2);
+                    vec.copyTo(gridPoint->vm.maxs);
+                }
+            }
+        }
+    }
 }
 
-qboolean cMoveGrid::CheckStuck
-	(
-	void
-	)
+void cMoveGrid::Move(void)
 {
-	int x;
-	int y;
-	int z;
-	trace_t trace;
+    gridpoint_t *gridPoint;
+    int          x, y, z;
+    Vector       vBoxSize;
+    Vector       old_origin;
+    float        best;
+    float        bx, by, bz;
+    Vector       bestdelta;
 
-	for( x = 0; x < m_iXRes; x++ )
-	{
-		for( y = 0; y < m_iYRes; y++ )
-		{
-			for( z = 0; z < m_iZRes; z++ )
-			{
-				gi.trace( &trace, v.vs->origin, v.mins, v.maxs, v.vs->origin, v.vs->entityNum, v.tracemask, false, false );
+    old_origin = v.vs->origin;
+    vBoxSize   = Vector(v.maxs) - Vector(v.mins);
 
-				if( trace.allsolid || trace.startsolid || trace.fraction == 0.0f )
-				{
-					return true;
-				}
-			}
-		}
-	}
+    CalculateBoxPoints();
 
-	return false;
+    for (x = 0; x < m_iXRes; x++) {
+        for (y = 0; y < m_iYRes; y++) {
+            for (z = 0; z < m_iZRes; z++) {
+                gridPoint = GetGridPoint(x, y, z);
+
+                gridPoint->origindelta = gridPoint->origin;
+                gridPoint->origin += old_origin;
+                gridPoint->origin.copyTo(gridPoint->vm.vs->origin);
+
+                G_DebugBBox(gridPoint->origin, gridPoint->vm.mins, gridPoint->vm.maxs, 1, 1, 0, 1);
+
+                VmoveSingle(&gridPoint->vm);
+
+                gridPoint->neworigin = gridPoint->vm.vs->origin;
+                gridPoint->changed   = gridPoint->neworigin - gridPoint->origin;
+                gridPoint->newvel    = gridPoint->vm.vs->velocity;
+                // set the ground entity
+                gridPoint->groundentity = NULL;
+                if (gridPoint->vm.vs->groundEntityNum != ENTITYNUM_NONE) {
+                    gridPoint->groundentity = &g_entities[gridPoint->vm.vs->groundEntityNum];
+                }
+            }
+        }
+    }
+
+    best = 99999;
+    bx = by = bz = 0;
+
+    for (x = 0; x < m_iXRes; x++) {
+        for (y = 0; y < m_iYRes; y++) {
+            for (z = 0; z < m_iZRes; z++) {
+                gridPoint = GetGridPoint(x, y, z);
+                float speed;
+
+                speed = (gridPoint->changed - gridPoint->origindelta).length();
+                if (speed < best) {
+                    best = gridPoint->changed.length();
+                    bx   = x;
+                    by   = y;
+                    bz   = z;
+                }
+            }
+        }
+    }
+
+    gridPoint = GetGridPoint(bx, by, bz);
+    bestdelta = gridPoint->neworigin - gridPoint->origindelta;
+    bestdelta.copyTo(v.vs->origin);
+    gridPoint->newvel.copyTo(v.vs->velocity);
 }
 
-void cMoveGrid::GetMoveInfo
-	(
-	vmove_t *vm
-	)
+qboolean cMoveGrid::CheckStuck(void)
 {
-	memcpy( vm, &v, sizeof( vmove_t ) );
+    int     x;
+    int     y;
+    int     z;
+    trace_t trace;
+
+    for (x = 0; x < m_iXRes; x++) {
+        for (y = 0; y < m_iYRes; y++) {
+            for (z = 0; z < m_iZRes; z++) {
+                gi.trace(
+                    &trace, v.vs->origin, v.mins, v.maxs, v.vs->origin, v.vs->entityNum, v.tracemask, false, false
+                );
+
+                if (trace.allsolid || trace.startsolid || trace.fraction == 0.0f) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
+void cMoveGrid::Archive(Archiver& arc)
+{
+    arc.ArchiveInteger(&m_iXRes);
+    arc.ArchiveInteger(&m_iYRes);
+    arc.ArchiveInteger(&m_iZRes);
+    arc.ArchiveRaw(&v, sizeof(vmove_t));
+    arc.ArchiveVec3(orientation[0]);
+    arc.ArchiveVec3(orientation[1]);
+    arc.ArchiveVec3(orientation[2]);
+
+    if (arc.Loading()) {
+        if (GridPoints) {
+            if (m_iXRes != 3 && m_iYRes != 3 && m_iZRes != 1) {
+                gi.Free(GridPoints);
+                GridPoints = (gridpoint_t *)gi.Malloc(sizeof(gridpoint_t) * m_iXRes * m_iYRes * m_iZRes);
+            }
+        }
+    }
+}
--- code/fgame/movegrid.h
+++ code/fgame/movegrid.h
@@ -23,100 +23,71 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // movegrid.h: Move grid
 //
 
-#ifndef __MOVEGRID_H__
-#define __MOVEGRID_H__
+#pragma once
 
 #include "g_local.h"
 #include "../qcommon/class.h"
 #include "../fgame/archive.h"
 
 typedef struct vehicleState_s {
-	float origin[ 3 ];
-	float velocity[ 3 ];
-	int groundEntityNum;
-	qboolean walking;
-	qboolean groundPlane;
-	trace_t groundTrace;
-	int entityNum;
-	float desired_dir[ 2 ];
-	qboolean hit_obstacle;
-	float hit_origin[ 3 ];
-	float obstacle_normal[ 3 ];
-	qboolean useGravity;
+    vec3_t   origin;
+    vec3_t   velocity;
+    int      groundEntityNum;
+    qboolean walking;
+    qboolean groundPlane;
+    trace_t  groundTrace;
+    int      entityNum;
+    vec2_t   desired_dir;
+    qboolean hit_obstacle;
+    float    hit_origin[3];
+    vec3_t   obstacle_normal;
+    qboolean useGravity;
 } vehicleState_t;
 
 typedef struct vmove_s {
-	vehicleState_t *vs;
-	float frametime;
-	float desired_speed;
-	int tracemask;
-	int numtouch;
-	int touchents[ 32 ];
-	float mins[ 3 ];
-	float maxs[ 3 ];
+    vehicleState_t *vs;
+    float           frametime;
+    float           desired_speed;
+    int             tracemask;
+    int             numtouch;
+    int             touchents[32];
+    vec3_t          mins;
+    vec3_t          maxs;
 } vmove_t;
 
 typedef struct gridpoint_s {
-	Vector origin;
-	Vector neworigin;
-	Vector origindelta;
-	Vector changed;
-	Vector newvel;
-	gentity_t *groundentity;
-	vmove_t vm;
-	vehicleState_t vs;
+    Vector         origin;
+    Vector         neworigin;
+    Vector         origindelta;
+    Vector         changed;
+    Vector         newvel;
+    gentity_t     *groundentity;
+    vmove_t        vm;
+    vehicleState_t vs;
 } gridpoint_t;
 
 class cMoveGrid : public Class
 {
 public:
-	gridpoint_t *GridPoints;
+    gridpoint_t *GridPoints;
 
 private:
-	vmove_t v;
-	float orientation[ 3 ][ 3 ];
-	int m_iXRes;
-	int m_iYRes;
-	int m_iZRes;
+    vmove_t v;
+    vec3_t  orientation[3];
+    int     m_iXRes;
+    int     m_iYRes;
+    int     m_iZRes;
 
 public:
-	cMoveGrid( int x, int y, int z );
-	virtual ~cMoveGrid();
+    cMoveGrid(int x, int y, int z);
+    virtual ~cMoveGrid();
 
-	void			SetOrientation( float( *v)[ 3 ] );
-	void			SetMoveInfo( vmove_t *vm );
-	void			CalculateBoxPoints( void );
-	gridpoint_t		*GetGridPoint( int x, int y, int z );
-	void			Move( void );
-	qboolean		CheckStuck( void );
-	void			GetMoveInfo( vmove_t *vm );
-	void			Archive( Archiver& arc );
+    void         SetOrientation(const vec3_t *v);
+    void         SetMoveInfo(vmove_t *vm);
+    void         CalculateBoxPoints(void);
+    gridpoint_t *GetGridPoint(int x, int y, int z);
+    void         Move(void);
+    qboolean     CheckStuck(void);
+    void         GetMoveInfo(vmove_t *vm);
+    void         Archive(Archiver        &arc);
 };
-
-inline void cMoveGrid::Archive
-	(
-	Archiver& arc
-	)
-{
-	arc.ArchiveInteger( &m_iXRes );
-	arc.ArchiveInteger( &m_iYRes );
-	arc.ArchiveInteger( &m_iZRes );
-	arc.ArchiveRaw( &v, sizeof( vmove_t ) );
-	arc.ArchiveVec3( orientation[ 0 ] );
-	arc.ArchiveVec3( orientation[ 1 ] );
-	arc.ArchiveVec3( orientation[ 2 ] );
-
-	if( arc.Loading() )
-	{
-		if( GridPoints )
-		{
-			if( m_iXRes != 3 && m_iYRes != 3 && m_iZRes != 1 )
-			{
-				gi.Free( GridPoints );
-				GridPoints = ( gridpoint_t * )gi.Malloc( sizeof( gridpoint_t ) * m_iXRes * m_iYRes * m_iZRes );
-			}
-		}
-	}
-}
-
-#endif
