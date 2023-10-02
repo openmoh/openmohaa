49aa2aa858045a6d1686f759a5debf858f795fe0
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-10-02 21:11:48 +0200

    Implemented UILabel from moh 2.0 and above

--- code/uilib/uilabel.cpp
+++ code/uilib/uilabel.cpp
@@ -31,8 +31,8 @@ Event EV_UILabel_LinkString
     "value string",
     "Creates a link from the specified value to a string. "
     "Use this if you want the label to display a string different from the value of the cvar"
-);
 
+);
 Event EV_UILabel_LinkCvarToShader
 (
     "linkcvartoshader",
@@ -41,14 +41,63 @@ Event EV_UILabel_LinkCvarToShader
     NULL,
     "Links the label's cvar to its shader"
 );
+// Added in 2.0
+//====
+Event EV_UILabel_OneShotSound
+(
+    "oneshotsound",
+    EV_DEFAULT,
+    "sf",
+    "soundName soundTime",
+    "Set the sound to play"
+);
+Event EV_UILabel_LoopingSound
+(
+    "loopingsound",
+    EV_DEFAULT,
+    "sffff",
+    "soundName start_volume hold_start_time ending_volume ramp_time",
+    "Set the sound to play"
+
+);
+Event EV_UILabel_OutlinedText
+(
+    "outlinedtext",
+    EV_DEFAULT,
+    "i",
+    "outlined",
+    "Set/clear outline text"
+);
+//====
+
+static const char *localizableVars[] = {
+    "cl_langamerefreshstatus",
+    "ui_timemessage",
+    "cg_obj_axistext1",
+    "cg_obj_axistext2",
+    "cg_obj_axistext3",
+    "cg_obj_axistext4",
+    "cg_obj_axistext5"
+    "cg_obj_alliedtext1",
+    "cg_obj_alliedtext2",
+    "cg_obj_alliedtext3",
+    "cg_obj_alliedtext4",
+    "cg_obj_alliedtext5",
+    "g_gametypestring",
+    "dm_serverstatus",
+    "com_errormessage",
+    NULL};
 
 CLASS_DECLARATION(UIWidget, UILabel, NULL) {
     {&W_MouseEntered,              &UILabel::MouseEntered         },
     {&W_MouseExited,               &UILabel::MouseExited          },
     {&EV_UILabel_LinkString,       &UILabel::LinkString           },
     {&EV_UILabel_LinkCvarToShader, &UILabel::SetLinkCvarToShader  },
+    {&EV_UILabel_OneShotSound,     &UILabel::OneShotSound         },
+    {&EV_UILabel_LoopingSound,     &UILabel::LoopingSound         },
     {&EV_Layout_Shader,            &UILabel::LabelLayoutShader    },
     {&EV_Layout_TileShader,        &UILabel::LabelLayoutTileShader},
+    {&EV_UILabel_OutlinedText,     &UILabel::EventSetOutlinedText },
     {NULL,                         NULL                           }
 };
 
@@ -56,6 +105,11 @@ UILabel::UILabel()
 {
     AllowActivate(false);
     m_bLinkCvarToShader = false;
+    m_bOneShotTriggered = false;
+    m_iNextOneShotTime  = 0;
+    m_iNextStateTime    = 0;
+    m_iLastDrawTime     = 0;
+    m_bOutlinedText     = false;
 }
 
 void UILabel::MouseEntered(Event *ev)
@@ -81,6 +135,11 @@ void UILabel::LinkString(Event *ev)
     ls = new linkstring(value, string);
 
     m_linkstrings.AddObject(ls);
+}
+
+void UILabel::SetLabel(str lab)
+{
+    label = lab;
 }
 
 int UILabel::FindLinkString(str val)
@@ -117,9 +176,54 @@ void UILabel::SetLinkCvarToShader(Event *ev)
     m_bLinkCvarToShader = true;
 }
 
-void UILabel::SetLabel(str lab)
+void UILabel::OneShotSound(Event *ev)
 {
-    label = lab;
+    m_sOneShotSoundName = ev->GetString(1);
+    m_fOneShotSoundTime = ev->GetFloat(2);
+}
+
+void UILabel::LoopingSound(Event *ev)
+{
+    m_sLoopingSoundName = ev->GetString(1);
+
+    switch (ev->NumArgs()) {
+    case 1:
+        m_fLoopingSoundHoldStartTime = 0.0;
+        m_fLoopingSoundStartVolume   = 1.0;
+        m_fLoopingSoundEndVolume     = 1.0;
+        m_fLoopingSoundRampTime      = 0.0;
+        break;
+    case 2:
+    case 3:
+        m_fLoopingSoundStartVolume   = ev->GetFloat(2);
+        m_fLoopingSoundHoldStartTime = 0.0;
+        m_fLoopingSoundEndVolume     = ev->GetFloat(2);
+        m_fLoopingSoundRampTime      = 0.0;
+        break;
+    case 4:
+        m_fLoopingSoundStartVolume   = ev->GetFloat(2);
+        m_fLoopingSoundHoldStartTime = ev->GetFloat(3);
+        m_fLoopingSoundEndVolume     = ev->GetFloat(4);
+        m_fLoopingSoundRampTime      = 4.0;
+        break;
+    case 5:
+        m_fLoopingSoundStartVolume   = ev->GetFloat(2);
+        m_fLoopingSoundHoldStartTime = ev->GetFloat(3);
+        m_fLoopingSoundEndVolume     = ev->GetFloat(4);
+        m_fLoopingSoundRampTime      = ev->GetFloat(5);
+        break;
+    default:
+        break;
+    }
+}
+
+void UILabel::EventSetOutlinedText(Event *ev)
+{
+    if (ev->NumArgs() > 0) {
+        m_bOutlinedText = ev->GetInteger(1) ? true : false;
+    } else {
+        m_bOutlinedText = true;
+    }
 }
 
 void UILabel::Draw(void)
@@ -127,8 +231,21 @@ void UILabel::Draw(void)
     const char *string;
     str         val;
 
+    ProcessSounds();
+
+    m_iLastDrawTime = uii.Sys_Milliseconds();
+
     if (m_cvarname.length()) {
         string = uii.Cvar_GetString(m_cvarname, "");
+
+        //
+        // localize the cvar value when possible
+        //
+        for (int i = 0; localizableVars[i]; i++) {
+            if (!strcmp(localizableVars[i], string)) {
+                string = Sys_LV_CL_ConvertString(localizableVars[i]);
+            }
+        }
 
         if (m_linkstrings.NumObjects()) {
             cvar_t *cvar = UI_FindCvar(m_cvarname);
@@ -180,6 +297,7 @@ void UILabel::Draw(void)
 
     if (*string) {
         float nr, ng, nb;
+        float alpha;
 
         nr = m_foreground_color.r;
         ng = m_foreground_color.g;
@@ -187,13 +305,118 @@ void UILabel::Draw(void)
 
         m_font->setColor(UColor(nr, ng, nb, m_foreground_color.a * m_local_alpha));
 
-        // print the text
-        m_font->PrintJustified(
-            getClientFrame(),
-            m_iFontAlignmentHorizontal,
-            m_iFontAlignmentVertical,
-            Sys_LV_CL_ConvertString(string),
-            m_bVirtual ? m_vVirtualScale : NULL
-        );
+        if (m_bOutlinedText) {
+            // print the text
+            m_font->PrintOutlinedJustified(
+                getClientFrame(),
+                m_iFontAlignmentHorizontal,
+                m_iFontAlignmentVertical,
+                string,
+                UBlack,
+                m_bVirtual ? m_vVirtualScale : NULL
+            );
+        } else {
+            // print the text
+            m_font->PrintJustified(
+                getClientFrame(),
+                m_iFontAlignmentHorizontal,
+                m_iFontAlignmentVertical,
+                string,
+                m_bVirtual ? m_vVirtualScale : NULL
+            );
+        }
+    }
+}
+
+void UILabel::FrameInitialized()
+{
+    if (m_sOneShotSoundName.length() || m_sLoopingSoundName.length()) {
+        uii.Cmd_Stuff("tmstop\n");
+        m_bOneShotTriggered = false;
+        m_iNextOneShotTime  = uii.Sys_Milliseconds() + 2000;
+        m_iNextStateTime    = 0;
+    }
+
+    m_iLastDrawTime = 0;
+
+    UIWidget::FrameInitialized();
+}
+
+void UILabel::ProcessSounds()
+{
+    char  buffer[64];
+    float frac;
+
+    if (paused->integer) {
+        if (m_iNextOneShotTime) {
+            m_iNextOneShotTime += uii.Sys_Milliseconds() - m_iLastDrawTime;
+        }
+
+        if (m_iNextStateTime) {
+            m_iNextStateTime += uii.Sys_Milliseconds() - m_iLastDrawTime;
+        }
+    }
+
+    if (m_iNextOneShotTime && uii.Sys_Milliseconds() > m_iNextOneShotTime) {
+        if (m_sOneShotSoundName.length() && !m_bOneShotTriggered) {
+            //
+            // use the looping sound if the oneshot sound was triggered is non-existent
+            //
+            uii.Snd_StopAllSound();
+            uii.Snd_FadeSound(0);
+            uii.Snd_PlaySoundDialog(m_sOneShotSoundName);
+
+            m_bOneShotTriggered = true;
+            m_iNextOneShotTime  = Sys_Milliseconds() + m_fOneShotSoundTime * 1000.0f;
+        } else if (m_sLoopingSoundName.length()) {
+            uii.Cmd_Stuff("tmstartloop " + m_sLoopingSoundName);
+
+            m_iNextOneShotTime = 0;
+            m_iSoundState      = 0;
+            m_iNextStateTime   = uii.Sys_Milliseconds();
+        } else {
+            m_iNextOneShotTime = 0;
+        }
+    }
+
+    if (m_iNextStateTime && uii.Sys_Milliseconds() >= m_iNextStateTime) {
+        switch (m_iSoundState) {
+        case 0:
+            Com_sprintf(buffer, sizeof(buffer), "tmvolume %4.2f\n", m_fLoopingSoundStartVolume);
+            uii.Cmd_Stuff(buffer);
+            m_iNextStateTime = uii.Sys_Milliseconds() + m_fLoopingSoundHoldStartTime * 1000;
+            m_iSoundState    = 1;
+            break;
+        case 1:
+            m_iLastEnterTime = m_iNextStateTime;
+            m_iSoundState    = 2;
+            m_iNextStateTime = uii.Sys_Milliseconds() + 10;
+            break;
+        case 2:
+            if (m_fLoopingSoundRampTime > 0) {
+                frac = (uii.Sys_Milliseconds() - m_iLastEnterTime) / (m_fLoopingSoundRampTime * 1000);
+            } else {
+                frac = 1.0;
+            }
+
+            Com_sprintf(
+                buffer,
+                sizeof(buffer),
+                "tmvolume %4.2f\n",
+                frac * (m_fLoopingSoundEndVolume - m_fLoopingSoundStartVolume) + m_fLoopingSoundStartVolume
+            );
+            uii.Cmd_Stuff(buffer);
+
+            m_iNextStateTime = uii.Sys_Milliseconds() + 10;
+            if (frac > 1.0) {
+                m_iSoundState = 3;
+            }
+            break;
+        case 3:
+            m_iNextStateTime = 0;
+            break;
+        default:
+            break;
+        }
     }
 }
--- code/uilib/uilabel.h
+++ code/uilib/uilabel.h
@@ -22,48 +22,66 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #pragma once
 
-class linkstring {
+class linkstring
+{
 public:
-	str value;
-	str string;
+    str value;
+    str string;
 
-	linkstring( str value, str string );
+    linkstring(str value, str string);
 };
 
-inline
-linkstring::linkstring
-	(
-	str value,
-	str string
-	)
+inline linkstring::linkstring(str value, str string)
 
 {
-	this->value = value;
-	this->string = string;
+    this->value  = value;
+    this->string = string;
 }
 
-class UILabel : public UIWidget {
-	str label;
-	Container<linkstring *> m_linkstrings;
-	qboolean m_bLinkCvarToShader;
-	str m_sCurrentShaderName;
+class UILabel : public UIWidget
+{
+    str                     label;
+    Container<linkstring *> m_linkstrings;
+    qboolean                m_bLinkCvarToShader;
+    str                     m_sCurrentShaderName;
+    str                     m_sOneShotSoundName;
+    str                     m_sLoopingSoundName;
+    qboolean                m_bOneShotTriggered;
+    float                   m_fOneShotSoundTime;
+    int                     m_iNextOneShotTime;
+    float                   m_fLoopingSoundStartVolume;
+    float                   m_fLoopingSoundHoldStartTime;
+    float                   m_fLoopingSoundEndVolume;
+    float                   m_fLoopingSoundRampTime;
+    int                     m_iNextStateTime;
+    int                     m_iSoundState;
+    int                     m_iLastEnterTime;
+    int                     m_iLastDrawTime;
+    qboolean                m_bOutlinedText;
 
 public:
-	CLASS_PROTOTYPE( UILabel );
+    CLASS_PROTOTYPE(UILabel);
 
 private:
-	void		MouseEntered( Event *ev );
-	void		MouseExited( Event *ev );
-	void		LinkString( Event *ev );
-	int			FindLinkString( str val );
-	void		LabelLayoutShader( Event *ev );
-	void		LabelLayoutTileShader( Event *ev );
-	void		SetLinkCvarToShader( Event *ev );
+    void MouseEntered(Event *ev);
+    void MouseExited(Event *ev);
+    void LinkString(Event *ev);
+    int  FindLinkString(str val);
+    void LabelLayoutShader(Event *ev);
+    void LabelLayoutTileShader(Event *ev);
+    // Added in 2.0
+    //====
+    void OneShotSound(Event *ev);
+    void LoopingSound(Event *ev);
+    void EventSetOutlinedText(Event *ev);
+    //====
+    void SetLinkCvarToShader(Event *ev);
+    void FrameInitialized() override; // Added in 2.0
+    void ProcessSounds();             // Added in 2.0
 
 public:
-	UILabel();
+    UILabel();
 
-	void		SetLabel( str lab );
-	void		Draw( void ) override;
+    void SetLabel(str lab);
+    void Draw(void) override;
 };
-
