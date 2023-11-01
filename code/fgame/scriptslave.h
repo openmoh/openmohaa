6616bfe53e2e7bec7f810ff8f7bd1ca082b9e772
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-11-01 22:21:09 +0100

    Formatted document

--- code/fgame/scriptslave.h
+++ code/fgame/scriptslave.h
@@ -1,6 +1,6 @@
 /*
 ===========================================================================
-Copyright (C) 2015 the OpenMoHAA team
+Copyright (C) 2023 the OpenMoHAA team
 
 This file is part of OpenMoHAA source code.
 
@@ -27,10 +27,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // perform several different relative and specific rotations and translations
 // and can cause other parts of the script to be executed when touched, damaged,
 // touched, or used.
-// 
+//
 
-#ifndef __SCRIPTSLAVE_H__
-#define __SCRIPTSLAVE_H__
+#pragma once
 
 #include "g_local.h"
 #include "entity.h"
@@ -44,235 +43,209 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 class ScriptSlave : public Mover
 {
-	protected:
-		float attack_finished;
+protected:
+    float attack_finished;
 
-		int dmg;
-		int dmg_means_of_death;
+    int dmg;
+    int dmg_means_of_death;
 
-	public:
-		qboolean commandswaiting;
+public:
+    qboolean commandswaiting;
 
-		Vector	TotalRotation;
-		Vector	NewAngles;
-		Vector	NewPos;
-		Vector	ForwardDir;
-		float	speed;
+    Vector TotalRotation;
+    Vector NewAngles;
+    Vector NewPos;
+    Vector ForwardDir;
+    float  speed;
 
-		Waypoint	*waypoint;
-		float		traveltime;
-		BSpline		*splinePath;
-		float		splineTime;
+    Waypoint *waypoint;
+    float     traveltime;
+    BSpline  *splinePath;
+    float     splineTime;
 
-		cSpline<4, 512>	*m_pCurPath;
-		int				m_iCurNode;
+    cSpline<4, 512> *m_pCurPath;
+    int              m_iCurNode;
 
-		float	m_fCurSpeed;
-		float	m_fIdealSpeed;
-		Vector	m_vIdealPosition;
-		Vector	m_vIdealDir;
-		float	m_fIdealAccel;
-		float	m_fIdealDistance;
-		float	m_fLookAhead;
+    float  m_fCurSpeed;
+    float  m_fIdealSpeed;
+    Vector m_vIdealPosition;
+    Vector m_vIdealDir;
+    float  m_fIdealAccel;
+    float  m_fIdealDistance;
+    float  m_fLookAhead;
 
-		qboolean	splineangles;
-		qboolean	ignoreangles;
-		qboolean	ignorevelocity;
-		qboolean	moving;  // is the script object currently moving?
+    qboolean splineangles;
+    qboolean ignoreangles;
+    qboolean ignorevelocity;
+    qboolean moving; // is the script object currently moving?
 
-	protected:
-		void				CheckNewOrders( void );
-		void				NewMove( void );
+protected:
+    void CheckNewOrders(void);
+    void NewMove(void);
 
+    CLASS_PROTOTYPE(ScriptSlave);
 
-      CLASS_PROTOTYPE( ScriptSlave );
+    ScriptSlave();
+    ~ScriptSlave();
 
-								ScriptSlave();
-								~ScriptSlave();
+    void BindEvent(Event *ev);
+    void EventUnbind(Event *ev);
+    void DoMove(Event *ev);
+    void WaitMove(Event *ev);
+    void MoveEnd(Event *ev);
+    void SetAnglesEvent(Event *ev);
+    void SetAngleEvent(Event *ev);
+    void SetModelEvent(Event *ev);
+    void TriggerEvent(Event *ev) override;
+    void GotoNextWaypoint(Event *ev);
+    void JumpTo(Event *ev);
+    void MoveToEvent(Event *ev);
+    void SetSpeed(Event *ev);
+    void SetTime(Event *ev);
+    void MoveUp(Event *ev);
+    void MoveDown(Event *ev);
+    void MoveNorth(Event *ev);
+    void MoveSouth(Event *ev);
+    void MoveEast(Event *ev);
+    void MoveWest(Event *ev);
+    void MoveForward(Event *ev);
+    void MoveBackward(Event *ev);
+    void MoveLeft(Event *ev);
+    void MoveRight(Event *ev);
+    void RotateXdownto(Event *ev);
+    void RotateYdownto(Event *ev);
+    void RotateZdownto(Event *ev);
+    void RotateAxisdownto(Event *ev);
+    void RotateXupto(Event *ev);
+    void RotateYupto(Event *ev);
+    void RotateZupto(Event *ev);
+    void RotateAxisupto(Event *ev);
+    void Rotateupto(Event *ev);
+    void Rotatedownto(Event *ev);
+    void Rotateto(Event *ev);
+    void RotateXdown(Event *ev);
+    void RotateYdown(Event *ev);
+    void RotateZdown(Event *ev);
+    void RotateAxisdown(Event *ev);
+    void RotateXup(Event *ev);
+    void RotateYup(Event *ev);
+    void RotateZup(Event *ev);
+    void RotateAxisup(Event *ev);
+    void RotateX(Event *ev);
+    void RotateY(Event *ev);
+    void RotateZ(Event *ev);
+    void RotateAxis(Event *ev);
+    void SetDamage(Event *ev);
+    void SetMeansOfDeath(Event *ev);
+    void FollowPath(Event *ev);
+    void EndPath(Event *ev);
+    void FollowingPath(Event *ev);
+    void CreatePath(SplinePath *path, splinetype_t type);
+    void Explode(Event *ev);
+    void NotShootable(Event *ev);
+    void OpenPortal(Event *ev);
+    void ClosePortal(Event *ev);
+    void PhysicsOn(Event *ev);
+    void PhysicsOff(Event *ev);
+    void PhysicsVelocity(Event *ev);
+    void DamageFunc(Event *ev);
+    void EventFlyPath(Event *ev);
+    void EventModifyFlyPath(Event *ev);
+    void SetupPath(cSpline<4, 512> *pPath, SimpleEntity *se);
 
-		void					BindEvent( Event *ev );
-		void					EventUnbind( Event *ev );
-		void					DoMove( Event *ev );
-		void					WaitMove( Event *ev );
-		void					MoveEnd( Event *ev );
-		void					SetAnglesEvent( Event *ev );
-		void					SetAngleEvent( Event *ev );
-      void              SetModelEvent( Event *ev );
-		void					TriggerEvent( Event *ev ) override;
-		void					GotoNextWaypoint( Event *ev );
-		void					JumpTo( Event *ev );
-		void					MoveToEvent( Event *ev );
-		void					SetSpeed( Event *ev );
-		void					SetTime( Event *ev );
-		void					MoveUp( Event *ev );
-		void					MoveDown( Event *ev );
-		void					MoveNorth( Event *ev );
-		void					MoveSouth( Event *ev );
-		void					MoveEast( Event *ev );
-		void					MoveWest( Event *ev );
-		void					MoveForward( Event *ev );
-		void					MoveBackward( Event *ev );
-		void					MoveLeft( Event *ev );
-		void					MoveRight( Event *ev );
-		void					RotateXdownto( Event *ev );
-		void					RotateYdownto( Event *ev );
-		void					RotateZdownto( Event *ev );
-		void					RotateAxisdownto( Event *ev );
-		void					RotateXupto( Event *ev );
-		void					RotateYupto( Event *ev );
-		void					RotateZupto( Event *ev );
-		void					RotateAxisupto( Event *ev );
-		void					Rotateupto( Event *ev );
-		void					Rotatedownto( Event *ev );
-		void					Rotateto( Event *ev );
-		void					RotateXdown( Event *ev );
-		void					RotateYdown( Event *ev );
-		void					RotateZdown( Event *ev );
-		void					RotateAxisdown( Event *ev );
-		void					RotateXup( Event *ev );
-		void					RotateYup( Event *ev );
-		void					RotateZup( Event *ev );
-		void					RotateAxisup( Event *ev );
-		void					RotateX( Event *ev );
-		void					RotateY( Event *ev );
-		void					RotateZ( Event *ev );
-		void					RotateAxis( Event *ev );
-      void					SetDamage( Event *ev );
-		void					SetMeansOfDeath( Event *ev );
-		void					FollowPath( Event *ev );
-		void					EndPath( Event *ev );
-		void					FollowingPath( Event *ev );
-      void              CreatePath( SplinePath *path,	splinetype_t type );
-      void              Explode( Event *ev );
-      void              NotShootable( Event *ev );
-      void              OpenPortal( Event *ev );
-      void              ClosePortal( Event *ev );
-      void              PhysicsOn( Event *ev );
-      void              PhysicsOff( Event *ev );
-      void              PhysicsVelocity( Event *ev );
-	  void				DamageFunc( Event *ev );
-	  void				EventFlyPath( Event *ev );
-	  void				EventModifyFlyPath( Event *ev );
-	  void				SetupPath( cSpline< 4, 512 > *pPath, SimpleEntity *se );
+    void Archive(Archiver& arc) override;
+};
 
-      void Archive( Archiver &arc ) override;
-	};
-
-inline void ScriptSlave::Archive
-	(
-	Archiver &arc
-	)
+inline void ScriptSlave::Archive(Archiver& arc)
 {
-	int tempInt;
+    int tempInt;
 
-	Mover::Archive( arc );
+    Mover::Archive(arc);
 
-	arc.ArchiveFloat( &attack_finished );
-	arc.ArchiveInteger( &dmg );
-	arc.ArchiveInteger( &dmg_means_of_death );
+    arc.ArchiveFloat(&attack_finished);
+    arc.ArchiveInteger(&dmg);
+    arc.ArchiveInteger(&dmg_means_of_death);
 
-	arc.ArchiveBoolean( &commandswaiting );
-	arc.ArchiveVector( &TotalRotation );
-	arc.ArchiveVector( &NewAngles );
-	arc.ArchiveVector( &NewPos );
-	arc.ArchiveVector( &ForwardDir );
-	arc.ArchiveFloat( &speed );
-	arc.ArchiveObjectPointer( ( Class ** )&waypoint );
-	arc.ArchiveFloat( &traveltime );
+    arc.ArchiveBoolean(&commandswaiting);
+    arc.ArchiveVector(&TotalRotation);
+    arc.ArchiveVector(&NewAngles);
+    arc.ArchiveVector(&NewPos);
+    arc.ArchiveVector(&ForwardDir);
+    arc.ArchiveFloat(&speed);
+    arc.ArchiveObjectPointer((Class **)&waypoint);
+    arc.ArchiveFloat(&traveltime);
 
-	if( arc.Saving() )
-	{
-		// if it exists, archive it, otherwise place a special NULL ptr tag
-		if( splinePath )
-		{
-			tempInt = ARCHIVE_POINTER_VALID;
-		}
-		else
-		{
-			tempInt = ARCHIVE_POINTER_NULL;
-		}
-		arc.ArchiveInteger( &tempInt );
-		if( tempInt == ARCHIVE_POINTER_VALID )
-		{
-			splinePath->Archive( arc );
-		}
-	}
-	else
-	{
-		arc.ArchiveInteger( &tempInt );
-		if( tempInt == ARCHIVE_POINTER_VALID )
-		{
-			splinePath = new BSpline;
-			splinePath->Archive( arc );
-		}
-		else
-		{
-			splinePath = NULL;
-		}
-	}
-	arc.ArchiveFloat( &splineTime );
-	arc.ArchiveBoolean( &splineangles );
-	arc.ArchiveBoolean( &ignoreangles );
-	arc.ArchiveBoolean( &moving );
+    if (arc.Saving()) {
+        // if it exists, archive it, otherwise place a special NULL ptr tag
+        if (splinePath) {
+            tempInt = ARCHIVE_POINTER_VALID;
+        } else {
+            tempInt = ARCHIVE_POINTER_NULL;
+        }
+        arc.ArchiveInteger(&tempInt);
+        if (tempInt == ARCHIVE_POINTER_VALID) {
+            splinePath->Archive(arc);
+        }
+    } else {
+        arc.ArchiveInteger(&tempInt);
+        if (tempInt == ARCHIVE_POINTER_VALID) {
+            splinePath = new BSpline;
+            splinePath->Archive(arc);
+        } else {
+            splinePath = NULL;
+        }
+    }
+    arc.ArchiveFloat(&splineTime);
+    arc.ArchiveBoolean(&splineangles);
+    arc.ArchiveBoolean(&ignoreangles);
+    arc.ArchiveBoolean(&moving);
 
-	if( arc.Saving() )
-	{
-		// if it exists, archive it, otherwise place a special NULL ptr tag
-		if( m_pCurPath )
-		{
-			tempInt = ARCHIVE_POINTER_VALID;
-		}
-		else
-		{
-			tempInt = ARCHIVE_POINTER_NULL;
-		}
-		arc.ArchiveInteger( &tempInt );
-		if( tempInt == ARCHIVE_POINTER_VALID )
-		{
-			m_pCurPath->Archive( arc );
-		}
-	}
-	else
-	{
-		arc.ArchiveInteger( &tempInt );
-		if( tempInt == ARCHIVE_POINTER_VALID )
-		{
-			m_pCurPath = new cSpline< 4, 512 >;
-			m_pCurPath->Archive( arc );
-		}
-		else
-		{
-			m_pCurPath = NULL;
-		}
-	}
+    if (arc.Saving()) {
+        // if it exists, archive it, otherwise place a special NULL ptr tag
+        if (m_pCurPath) {
+            tempInt = ARCHIVE_POINTER_VALID;
+        } else {
+            tempInt = ARCHIVE_POINTER_NULL;
+        }
+        arc.ArchiveInteger(&tempInt);
+        if (tempInt == ARCHIVE_POINTER_VALID) {
+            m_pCurPath->Archive(arc);
+        }
+    } else {
+        arc.ArchiveInteger(&tempInt);
+        if (tempInt == ARCHIVE_POINTER_VALID) {
+            m_pCurPath = new cSpline<4, 512>;
+            m_pCurPath->Archive(arc);
+        } else {
+            m_pCurPath = NULL;
+        }
+    }
 }
 
 class ScriptModel : public ScriptSlave
-	{
-   private:
-      void              GibEvent(Event *ev);
+{
+private:
+    void GibEvent(Event *ev);
 
-   public:
-      CLASS_PROTOTYPE( ScriptModel );
+public:
+    CLASS_PROTOTYPE(ScriptModel);
 
-								ScriptModel();
-		void					SetAngleEvent( Event *ev );
-		void					SetModelEvent( Event *ev );
-		void					SetAnimEvent( Event *ev );
-	};
+    ScriptModel();
+    void SetAngleEvent(Event *ev);
+    void SetModelEvent(Event *ev);
+    void SetAnimEvent(Event *ev);
+};
 
 class ScriptOrigin : public ScriptSlave
-	{
-	public:
-      CLASS_PROTOTYPE( ScriptOrigin );
-								ScriptOrigin();
-	};
+{
+public:
+    CLASS_PROTOTYPE(ScriptOrigin);
+    ScriptOrigin();
+};
 
 class ScriptSkyOrigin : public ScriptSlave
-	{
-	public:
-      CLASS_PROTOTYPE( ScriptSkyOrigin );
-								ScriptSkyOrigin();
-	};
-
-#endif /* scriptslave.h */
+{
+public:
+    CLASS_PROTOTYPE(ScriptSkyOrigin);
+    ScriptSkyOrigin();
+};
