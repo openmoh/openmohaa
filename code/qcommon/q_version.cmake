9814887b10b9102a1b3402202e49364f2823ecd9
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-11-29 18:57:22 +0100

    Don't execute Git if the source directory is not a working Git repo

--- code/qcommon/q_version.cmake
+++ code/qcommon/q_version.cmake
@@ -1,36 +1,49 @@
 add_library(qcommon_version INTERFACE)
 
-# Current branch
 execute_process(
-	COMMAND git branch --show-current
+	COMMAND git rev-parse --is-inside-work-tree
 	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	OUTPUT_VARIABLE GIT_BRANCH_NAME
+	OUTPUT_VARIABLE GIT_IS_INSIDE_WORK
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-# Revision
-execute_process(
-	COMMAND git show -s --format=%H HEAD
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	OUTPUT_VARIABLE GIT_REVISION_HASH
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if ("${GIT_IS_INSIDE_WORK}" STREQUAL "true")
+	message(VERBOSE "Git was successfully detected in the source directory. Relevant Git information will be included in the game version.")
 
-# Revision
-execute_process(
-	COMMAND git show -s --format=%h HEAD
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	OUTPUT_VARIABLE GIT_REVISION_HASH_ABBREVIATED
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+	# Current branch
+	execute_process(
+		COMMAND git branch --show-current
+		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+		OUTPUT_VARIABLE GIT_BRANCH_NAME
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
 
-# Commit date
-execute_process(
-	COMMAND git show -s --format=%ct
-	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-	OUTPUT_VARIABLE GIT_REVISION_DATE
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+	# Revision
+	execute_process(
+		COMMAND git show -s --format=%H HEAD
+		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+		OUTPUT_VARIABLE GIT_REVISION_HASH
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+
+	# Revision
+	execute_process(
+		COMMAND git show -s --format=%h HEAD
+		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+		OUTPUT_VARIABLE GIT_REVISION_HASH_ABBREVIATED
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+
+	# Commit date
+	execute_process(
+		COMMAND git show -s --format=%ct
+		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+		OUTPUT_VARIABLE GIT_REVISION_DATE
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+else()
+	message(WARNING "The repository was not detected as a Git working directory. Git information will not be available in the game version number.")
+endif()
 
 string(TIMESTAMP GIT_REVISION_DATE_TIME UTC)
 
