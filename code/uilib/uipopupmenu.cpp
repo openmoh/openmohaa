ec71ff3c3cccbf365caa64019b745c01f9f64c2b
pryon <pryon@users.noreply.github.com>
2023-12-17 22:51:18 +0100

    Implement UIPopupMenu (#87)
    * Implement UIPopupMenu
    
    * Use `std::fmax` for floats

--- code/uilib/uipopupmenu.cpp
+++ code/uilib/uipopupmenu.cpp
@@ -1,6 +1,6 @@
 /*
 ===========================================================================
-Copyright (C) 2015 the OpenMoHAA team
+Copyright (C) 2015-2023 the OpenMoHAA team
 
 This file is part of OpenMoHAA source code.
 
@@ -21,10 +21,15 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "ui_local.h"
+#include "localization.h"
 
 CLASS_DECLARATION( UIWidget, UIPopupMenu, NULL )
 {
-	{ NULL, NULL }
+	{ &W_LeftMouseUp,	&UIPopupMenu::MouseReleased	},
+	{ &W_MouseMoved,	&UIPopupMenu::MouseMoved	},
+	{ &W_MouseExited,	&UIPopupMenu::MouseExited	},
+	{ &W_Deactivated,	&UIPopupMenu::DismissEvent	},
+	{ NULL,				NULL						}
 };
 
 uipopup_type UI_PopupTypeStringToInt(str type)
@@ -51,7 +56,36 @@ uipopup_type UI_PopupTypeStringToInt(str type)
 
 UIPopupMenu::UIPopupMenu()
 {
-	// FIXME: stub
+	m_selected = -1;
+	m_listener = NULL;
+	m_parentMenu = NULL;
+	m_submenu = -1;
+	m_marlett = UIFont("marlett");
+	m_autodismiss = qtrue;
+	m_describe = NULL;
+	m_submenuptr = NULL;
+
+	m_highlightFGColor = UColor(0.9f, 0.8f, 0.6f, 1.0f);
+	m_highlightBGColor = UColor(0.21f, 0.18f, 0.015f, 1.0f);
+	m_background_color = UColor(0.02f, 0.07f, 0.005f, 1.0f);
+	m_foreground_color = UHudColor;
+
+	m_iFontAlignmentHorizontal = FONT_JUSTHORZ_RIGHT;
+
+}
+
+UIPopupMenu::~UIPopupMenu()
+{
+	if (m_parentMenu)
+	{
+		m_submenu = -1;
+	}
+
+	if (m_submenu != -1 && m_submenuptr)
+	{
+		m_submenuptr->m_parentMenu = NULL;
+		delete m_submenuptr;
+	}
 }
 
 uipopup_describe *UIPopupMenu::getDescribeFromPoint
@@ -60,7 +94,25 @@ uipopup_describe *UIPopupMenu::getDescribeFromPoint
 	)
 
 {
-	// FIXME: stub
+	// FIXME: macro or inline for this somewhere?
+	if (p.y < 4.0f || p.x < 2.0f || m_frame.size.width - 2.0f <= p.x)
+	{
+		return NULL;
+	}
+
+	float top = m_vVirtualScale[1] * 4.0f;
+	for (int i = 1; i <= m_describe->NumObjects(); i++)
+	{
+		uipopup_describe* desc = m_describe->ObjectAt(i);
+		float height = getDescribeHeight(desc);
+		if (p.y >= top && p.y < top + height)
+		{
+			return desc;
+		}
+
+		top += height;
+	}
+
 	return NULL;
 }
 
@@ -70,7 +122,37 @@ void UIPopupMenu::MakeSubMenu
 	)
 
 {
-	// FIXME: stub
+	UIRect2D place;
+	for (int i = 1; i <= m_submenu; i++)
+	{
+		place.pos.y += getDescribeHeight(m_describe->ObjectAt(i));
+	}
+
+	place.size.height = getDescribeHeight(m_describe->ObjectAt(m_submenu)) + 8.0f;
+	place.size.width = m_frame.size.width - 40.0f;
+
+	place.pos.x = 20.0f;
+	if (place.size.width < 0.0f)
+	{
+		place.pos.x -= place.size.width * 0.5f;
+		place.size.width = 0.0f;
+	}
+
+	place.pos.x += m_screenframe.pos.x;
+	place.pos.y += m_screenframe.pos.y;
+
+	UIPopupMenu *me = new UIPopupMenu();
+	me->Create(
+		(Container<uipopup_describe*> *)m_describe->ObjectAt(m_submenu)->data,
+		m_listener,
+		place,
+		UIP_WHERE_RIGHT,
+		qfalse,
+		-1.0f
+	);
+
+	me->m_parentMenu = this;
+	m_submenuptr = me;
 }
 
 float UIPopupMenu::getDescribeHeight
@@ -79,8 +161,12 @@ float UIPopupMenu::getDescribeHeight
 	)
 
 {
-	// FIXME: stub
-	return 0.0f;
+	if (d->type == UIP_SEPARATOR)
+	{
+		return 8.0f;
+	}
+	
+	return m_font->getHeight(m_bVirtual);
 }
 
 float UIPopupMenu::getDescribeWidth
@@ -89,8 +175,7 @@ float UIPopupMenu::getDescribeWidth
 	)
 
 {
-	// FIXME: stub
-	return 0.0f;
+	return m_font->getWidth(d->title, -1);
 }
 
 bool UIPopupMenu::MouseInSubmenus
@@ -99,8 +184,17 @@ bool UIPopupMenu::MouseInSubmenus
 	)
 
 {
-	// FIXME: stub
-	return false;
+	if (m_submenu == -1)
+	{
+		return false;
+	}
+	
+	if (m_submenuptr->m_screenframe.contains(uid.mouseX, uid.mouseY))
+	{
+		return true;
+	}
+
+	return m_submenuptr->MouseInSubmenus();
 }
 
 
@@ -112,7 +206,7 @@ void UIPopupMenu::Create
 	)
 
 {
-	// FIXME: stub
+	Create(describe, listener, UIRect2D(where.x, where.y, 0.0f, 0.0f), UIP_WHERE_RIGHT, qfalse, -1.0f);
 }
 
 void UIPopupMenu::Create
@@ -126,7 +220,95 @@ void UIPopupMenu::Create
 	)
 
 {
-	// FIXME: stub
+	m_describe = describe;
+	m_listener = listener;
+	setFont("verdana-12");
+
+	if (bVirtualSize)
+	{
+		m_bVirtual = qtrue;
+		m_vVirtualScale[0] = (float)uid.vidWidth / SCREEN_WIDTH;
+		m_vVirtualScale[1] = (float)uid.vidHeight / SCREEN_HEIGHT;
+	}
+
+	if (!(m_describe && m_listener))
+	{
+		return;
+	}
+
+	UISize2D totalSize;
+	for (int i = 1; i <= m_describe->NumObjects(); i++)
+	{
+		uipopup_describe* desc = m_describe->ObjectAt(i);
+		totalSize.height += getDescribeHeight(desc);
+		if (desc->type != UIP_SEPARATOR)
+		{
+			float newWidth = getDescribeWidth(desc);
+			if (newWidth > totalSize.width)
+			{
+				totalSize.width = newWidth;
+			}
+		}
+	}
+
+	totalSize.height /= m_vVirtualScale[1];
+
+	if (width != -1.0f)
+	{
+		totalSize.width = width;
+	}
+
+	if (totalSize.width == 0.0f || totalSize.height == 0.0f)
+	{
+		return;
+	}
+
+	totalSize.width += 24.0f;
+	totalSize.height += 8.0f;
+
+	if (m_bVirtual)
+	{
+		totalSize.width *= m_vVirtualScale[0];
+		totalSize.height *= m_vVirtualScale[1];
+	}
+
+	UIPoint2D place;
+	place.x = (where) ? createRect.pos.x : createRect.size.width + createRect.pos.x;
+	place.y = (where) ? createRect.size.height + createRect.pos.y : createRect.pos.y;
+
+	// FIXME: avoid code repetition, extract these checks below to an inline func
+
+	// Adjust position of menu rect if it goes beyond the screen width
+	if (place.x + totalSize.width > uid.vidWidth)
+	{
+		place.x = (where) ? createRect.size.width + createRect.pos.x : createRect.pos.x;
+
+		if (place.x < totalSize.width)
+		{
+			place.x = std::fmax(0.0f, uid.vidWidth - totalSize.width);
+		}
+		else
+		{
+			place.x -= totalSize.width;
+		}
+	}
+
+	// Adjust position of menu rect if it goes beyond the screen height
+	if (place.y + totalSize.height > uid.vidHeight)
+	{
+		place.y = (where) ? createRect.pos.y : createRect.size.height + createRect.pos.y;
+
+		if (place.y < totalSize.height)
+		{
+			place.y = std::fmax(0.0f, uid.vidHeight - totalSize.height);
+		}
+		else
+		{
+			place.y -= totalSize.height;
+		}
+	}
+
+	InitFrame(NULL, place.x, place.y, totalSize.width, totalSize.height, 1);
 }
 
 void UIPopupMenu::YouAreASubmenu
@@ -135,7 +317,7 @@ void UIPopupMenu::YouAreASubmenu
 	)
 
 {
-	// FIXME: stub
+	m_parentMenu = me;
 }
 
 void UIPopupMenu::Draw
@@ -144,7 +326,65 @@ void UIPopupMenu::Draw
 	)
 
 {
-	// FIXME: stub
+	const float originX = m_vVirtualScale[0] * 4.0f;
+	const float originY = m_vVirtualScale[1] * 4.0f;
+
+	float fFontHeight = m_font->getHeight(m_bVirtual);
+	float top = originY;
+	for (int i = 1; i <= m_describe->NumObjects(); i++)
+	{
+		uipopup_describe* desc = m_describe->ObjectAt(i);
+		if (desc->type == UIP_SEPARATOR)
+		{
+			DrawBox(
+				originX * 2.0f,
+				originY + top,
+				m_frame.size.width - (originX * 4.0f),
+				1.0f,
+				m_foreground_color,
+				1.0f
+			);
+		}
+		else
+		{
+			UColor textColor;
+			if (m_selected == i || m_submenu == i)
+			{
+				DrawBox(
+					originX / 2.0f,
+					top,
+					m_frame.size.width - originX,
+					fFontHeight,
+					m_highlightBGColor,
+					m_local_alpha
+				);
+
+				textColor = m_highlightFGColor;
+			}
+			else
+			{
+				textColor = m_foreground_color;
+			}
+
+			m_font->setColor(textColor);
+			m_marlett.setColor(textColor);
+			const char *text = Sys_LV_CL_ConvertString(desc->title);
+			m_font->Print(4.0f, top / m_vVirtualScale[1], text, -1, m_bVirtual);
+
+			if (desc->type == UIP_SUBMENU)
+			{
+				m_marlett.PrintJustified(
+					UIRect2D(0.0f, top, m_frame.size.width - originX, getDescribeHeight(desc)),
+					m_iFontAlignmentHorizontal,
+					m_iFontAlignmentVertical,
+					"4",
+					m_bVirtual ? m_vVirtualScale : NULL
+				);
+			}
+		}
+
+		top += getDescribeHeight(desc);
+	}
 }
 
 void UIPopupMenu::MouseReleased
@@ -153,7 +393,38 @@ void UIPopupMenu::MouseReleased
 	)
 
 {
-	// FIXME: stub
+	UIPoint2D p = MouseEventToClientPoint(ev); // unused
+	if (m_selected == -1 || m_selected > m_describe->NumObjects())
+	{
+		return;
+	}
+
+	uipopup_describe *desc = m_describe->ObjectAt(m_selected);
+	if (!desc || desc->type == UIP_SEPARATOR || desc->type == UIP_SUBMENU)
+	{
+		return;
+	}
+
+	if (m_autodismiss)
+	{
+		Dismiss();
+	}
+
+	switch (desc->type)
+	{
+	case UIP_EVENT:
+		ProcessEvent((Event*)desc->data);
+		break;
+	case UIP_EVENT_STRING:
+		ProcessEvent(new Event((const char*)desc->data));
+		break;
+	case UIP_CMD:
+	case UIP_CVAR:
+		Cbuf_AddText((const char*)desc->data);
+		break;
+	default:
+		return;
+	}
 }
 
 void UIPopupMenu::MouseMoved
@@ -162,7 +433,17 @@ void UIPopupMenu::MouseMoved
 	)
 
 {
-	// FIXME: stub
+	UIPoint2D point = MouseEventToClientPoint(ev);
+	uipopup_describe* describe = getDescribeFromPoint(point);
+	if (describe)
+	{
+		int index = m_describe->IndexOfObject(describe);
+		setSelection(index);
+	}
+	else
+	{
+		setSelection(-1);
+	}
 }
 
 void UIPopupMenu::MouseExited
@@ -171,7 +452,7 @@ void UIPopupMenu::MouseExited
 	)
 
 {
-	// FIXME: stub
+	MouseMoved(ev);
 }
 
 void UIPopupMenu::Dismiss
@@ -180,7 +461,16 @@ void UIPopupMenu::Dismiss
 	)
 
 {
-	// FIXME: stub
+	// navigate to the topmost menu
+	if (m_parentMenu)
+	{
+		m_parentMenu->Dismiss();
+	}
+	else
+	{
+		// this is the topmost menu, self-destruct
+		delete this;
+	}
 }
 
 void UIPopupMenu::DismissEvent
@@ -189,7 +479,7 @@ void UIPopupMenu::DismissEvent
 	)
 
 {
-	// FIXME: stub
+	Dismiss();
 }
 
 void UIPopupMenu::setAutoDismiss
@@ -198,7 +488,7 @@ void UIPopupMenu::setAutoDismiss
 	)
 
 {
-	// FIXME: stub
+	m_autodismiss = b;
 }
 
 void UIPopupMenu::setNextSelection
@@ -207,7 +497,25 @@ void UIPopupMenu::setNextSelection
 	)
 
 {
-	// FIXME: stub
+	if (!m_describe->NumObjects())
+	{
+		return;
+	}
+
+	if (m_selected == -1)
+	{
+		setSelection(0);
+		return;
+	}
+
+	int sel = m_selected + 1;
+	if (sel > m_describe->NumObjects())
+	{
+		// wrap around
+		sel = 0;
+	}
+	
+	setSelection(sel);
 }
 
 void UIPopupMenu::setPrevSelection
@@ -216,7 +524,29 @@ void UIPopupMenu::setPrevSelection
 	)
 
 {
-	// FIXME: stub
+	if (!m_describe->NumObjects())
+	{
+		return;
+	}
+
+	if (m_selected == -1)
+	{
+		setSelection(0);
+		return;
+	}
+
+	int sel;
+	if (m_selected != 0)
+	{
+		sel = m_selected - 1;
+	}
+	else
+	{
+		// wrap around
+		sel = m_describe->NumObjects();
+	}
+
+	setSelection(sel);
 }
 
 uipopup_describe *UIPopupMenu::getSelectedDescribe
@@ -225,8 +555,12 @@ uipopup_describe *UIPopupMenu::getSelectedDescribe
 	)
 
 {
-	// FIXME: stub
-	return NULL;
+	if (m_selected == -1)
+	{
+		return NULL;
+	}
+
+	return m_describe->ObjectAt(m_selected);
 }
 
 void UIPopupMenu::setSelection
@@ -235,7 +569,49 @@ void UIPopupMenu::setSelection
 	)
 
 {
-	// FIXME: stub
+	int maxSel = m_describe->NumObjects();
+	if (sel > maxSel)
+	{
+		m_selected = maxSel;
+		return;
+	}
+
+	int newSubMenu = -1;
+	if (sel != -1)
+	{
+		uipopup_describe* desc = m_describe->ObjectAt(sel);
+		if (desc && desc->type == UIP_SUBMENU)
+		{
+			newSubMenu = sel;
+		}
+	}
+
+	m_selected = sel;
+
+	if (newSubMenu == m_submenu)
+	{
+		return;
+	}
+
+	if (newSubMenu == -1)
+	{
+		if (m_submenuptr)
+		{
+			delete m_submenuptr;
+		}
+
+		m_submenu = -1;
+	}
+	else
+	{
+		if (m_submenu != -1 && m_submenuptr)
+		{
+			delete m_submenuptr;
+		}
+
+		m_submenu = newSubMenu;
+		MakeSubMenu();
+	}
 }
 
 void UIPopupMenu::setHighlightFGColor
@@ -244,7 +620,7 @@ void UIPopupMenu::setHighlightFGColor
 	)
 
 {
-	// FIXME: stub
+	m_highlightFGColor = c;
 }
 
 void UIPopupMenu::setHighlightBGColor
@@ -253,7 +629,7 @@ void UIPopupMenu::setHighlightBGColor
 	)
 
 {
-	// FIXME: stub
+	m_highlightBGColor = c;
 }
 
 void UIPopupMenu::setPopupHighlightBGColor
@@ -263,7 +639,7 @@ void UIPopupMenu::setPopupHighlightBGColor
 	)
 
 {
-	// FIXME: stub
+	// not present in disassembly
 }
 
 void UIPopupMenu::setPopupHighlightFGColor
@@ -273,7 +649,7 @@ void UIPopupMenu::setPopupHighlightFGColor
 	)
 
 {
-	// FIXME: stub
+	// not present in disassembly
 }
 
 void UIPopupMenu::getPulldown
@@ -282,6 +658,6 @@ void UIPopupMenu::getPulldown
 	)
 
 {
-	// FIXME: stub
+	// not present in disassembly
 }
 
