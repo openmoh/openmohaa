3692671f4946e2c0ff452bf3f1a27d4406cff4f3
smallmodel <15067410+smallmodel@users.noreply.github.com>
2023-12-27 20:19:58 +0100

    Implemented bind_t and bindlistener

--- code/client/cl_uibind.cpp
+++ code/client/cl_uibind.cpp
@@ -24,7 +24,20 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 bind_t::bind_t()
 {
-    // FIXME: stub
+    int i;
+
+    commandwidth       = 100;
+    commandheight      = 20;
+    primarykeywidth    = 100;
+    primarykeyheight   = 20;
+    alternatekeywidth  = 100;
+    alternatekeyheight = 20;
+    fillwidth          = 16;
+    fillmaterial       = NULL;
+
+    for (i = 0; i < ARRAY_LEN(headermats); i++) {
+        headermats[i] = NULL;
+    }
 }
 
 bind_t::~bind_t()
@@ -34,7 +47,16 @@ bind_t::~bind_t()
 
 void bind_t::Clear(void)
 {
-    // FIXME: stub
+    for (int i = binds.NumObjects(); i > 0; i--) {
+        bind_item_t *bi;
+
+        bi = binds.ObjectAt(i);
+        binds.RemoveObjectAt(i);
+
+        if (bi) {
+            delete bi;
+        }
+    }
 }
 
 CLASS_DECLARATION(Listener, bindlistener, NULL) {
@@ -50,136 +72,234 @@ bindlistener::bindlistener(bind_t *b)
 
 void bindlistener::Header(Event *ev)
 {
-    // FIXME: stub
+    int num;
+
+    num = ev->GetInteger(1) - 1;
+    if (num > ARRAY_LEN(bind_t::headers)) {
+        return;
+    }
+
+    bind->headers[num] = ev->GetString(2);
+    if (ev->NumArgs() > 2) {
+        bind->headermats[num] = uWinMan.RegisterShader(ev->GetString(3));
+    }
 }
 
 void bindlistener::ChangeSound(Event *ev)
 {
-    // FIXME: stub
+    bind->changesound = ev->GetString(1);
 }
 
 void bindlistener::ActiveSound(Event *ev)
 {
-    // FIXME: stub
+    bind->activesound = ev->GetString(1);
 }
 
 void bindlistener::EnterSound(Event *ev)
 {
-    // FIXME: stub
+    bind->entersound = ev->GetString(1);
 }
 
 void bindlistener::SelectForegroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->selectfgcolor.r = ev->GetFloat(1);
+    bind->selectfgcolor.g = ev->GetFloat(2);
+    bind->selectfgcolor.b = ev->GetFloat(3);
+    bind->selectfgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::SelectBackgroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->selectbgcolor.r = ev->GetFloat(1);
+    bind->selectbgcolor.g = ev->GetFloat(2);
+    bind->selectbgcolor.b = ev->GetFloat(3);
+    bind->selectbgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::TitleForegroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->titlefgcolor.r = ev->GetFloat(1);
+    bind->titlefgcolor.g = ev->GetFloat(2);
+    bind->titlefgcolor.b = ev->GetFloat(3);
+    bind->titlefgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::TitleBackgroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->titlebgcolor.r = ev->GetFloat(1);
+    bind->titlebgcolor.g = ev->GetFloat(2);
+    bind->titlebgcolor.b = ev->GetFloat(3);
+    bind->titlebgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::ActiveBorderColor(Event *ev)
 {
-    // FIXME: stub
+    bind->activebordercolor.r = ev->GetFloat(1);
+    bind->activebordercolor.g = ev->GetFloat(2);
+    bind->activebordercolor.b = ev->GetFloat(3);
+    bind->activebordercolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::ActiveForegroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->activefgcolor.r = ev->GetFloat(1);
+    bind->activefgcolor.g = ev->GetFloat(2);
+    bind->activefgcolor.b = ev->GetFloat(3);
+    bind->activefgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::ActiveBackgroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->activebgcolor.r = ev->GetFloat(1);
+    bind->activebgcolor.g = ev->GetFloat(2);
+    bind->activebgcolor.b = ev->GetFloat(3);
+    bind->activebgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::InactiveForegroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->inactivefgcolor.r = ev->GetFloat(1);
+    bind->inactivefgcolor.g = ev->GetFloat(2);
+    bind->inactivefgcolor.b = ev->GetFloat(3);
+    bind->inactivefgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::InactiveBackgroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->inactivebgcolor.r = ev->GetFloat(1);
+    bind->inactivebgcolor.g = ev->GetFloat(2);
+    bind->inactivebgcolor.b = ev->GetFloat(3);
+    bind->inactivebgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::HighlightForegroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->highlightfgcolor.r = ev->GetFloat(1);
+    bind->highlightfgcolor.g = ev->GetFloat(2);
+    bind->highlightfgcolor.b = ev->GetFloat(3);
+    bind->highlightfgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::HighlightBackgroundColor(Event *ev)
 {
-    // FIXME: stub
+    bind->highlightbgcolor.r = ev->GetFloat(1);
+    bind->highlightbgcolor.g = ev->GetFloat(2);
+    bind->highlightbgcolor.b = ev->GetFloat(3);
+    bind->highlightbgcolor.a = ev->GetFloat(4);
 }
 
 void bindlistener::Align(Event *ev)
 {
-    // FIXME: stub
+    int i;
+
+    for (i = 1; i <= ev->NumArgs(); i++) {
+        str align = ev->GetString(i);
+
+        if (!str::icmp(align, "left")) {
+            bind->align |= WA_LEFT;
+        } else if (!str::icmp(align, "right")) {
+            bind->align |= WA_RIGHT;
+        } else if (!str::icmp(align, "top")) {
+            bind->align |= WA_TOP;
+        } else if (!str::icmp(align, "bottom")) {
+            bind->align |= WA_BOTTOM;
+        } else if (!str::icmp(align, "centerx")) {
+            bind->align |= WA_CENTERX;
+        } else if (!str::icmp(align, "centery")) {
+            bind->align |= WA_CENTERY;
+        }
+    }
 }
 
 void bindlistener::NewItem(Event *ev)
 {
-    // FIXME: stub
+    bind_item_t      *bt;
+    UIReggedMaterial *mat = NULL;
+
+    if (ev->NumArgs() > 2) {
+        mat = uWinMan.RegisterShader(ev->GetString(3));
+    }
+
+    str n = ev->GetString(1);
+    str c = ev->GetString(2);
+
+    bt               = new bind_item_t;
+    bt->name         = n;
+    bt->command      = c;
+    bt->nameMaterial = mat;
+
+    bind->binds.AddObject(bt);
 }
 
 void bindlistener::Width(Event *ev)
 {
-    // FIXME: stub
+    bind->width = ev->GetInteger(1);
 }
 
 void bindlistener::Height(Event *ev)
 {
-    // FIXME: stub
+    bind->height = ev->GetInteger(1);
 }
 
 void bindlistener::FillWidth(Event *ev)
 {
-    // FIXME: stub
+    bind->fillwidth = ev->GetInteger(1);
+
+    if (ev->NumArgs() > 1) {
+        bind->fillmaterial = uWinMan.RegisterShader(ev->GetString(2));
+    }
 }
 
 void bindlistener::CommandWidth(Event *ev)
 {
-    // FIXME: stub
+    bind->commandwidth = ev->GetInteger(1);
 }
 
 void bindlistener::CommandHeight(Event *ev)
 {
-    // FIXME: stub
+    bind->commandheight = ev->GetInteger(1);
 }
 
 void bindlistener::PrimaryKeyWidth(Event *ev)
 {
-    // FIXME: stub
+    bind->primarykeywidth = ev->GetInteger(1);
 }
 
 void bindlistener::PrimaryKeyHeight(Event *ev)
 {
-    // FIXME: stub
+    bind->primarykeyheight = ev->GetInteger(1);
 }
 
 void bindlistener::AlternateKeyWidth(Event *ev)
 {
-    // FIXME: stub
+    bind->alternatekeywidth = ev->GetInteger(1);
 }
 
 void bindlistener::AlternateKeyHeight(Event *ev)
 {
-    // FIXME: stub
+    bind->alternatekeyheight = ev->GetInteger(1);
 }
 
 bool bindlistener::Load(Script& script)
 {
-    // FIXME: stub
-    return false;
+    str token;
+    str errortext;
+
+    while (script.TokenAvailable(true)) {
+        token = script.GetToken(true);
+
+        if (!token.length() || !ValidEvent(token)) {
+            throw "invalid token";
+        }
+
+        Event *event = new Event(token);
+
+        while (script.TokenAvailable(false)) {
+            event->AddToken(script.GetToken(false));
+        }
+
+        ProcessEvent(event);
+    }
+
+    return true;
 }
