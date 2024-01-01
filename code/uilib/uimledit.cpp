7dc72b435e9fb7f6f7a8f05b964e8485e6cb3367
smallmodel <15067410+smallmodel@users.noreply.github.com>
2024-01-01 15:57:44 +0100

    Formatted multiline edit source file

--- code/uilib/uimledit.cpp
+++ code/uilib/uimledit.cpp
@@ -22,267 +22,185 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "ui_local.h"
 
-CLASS_DECLARATION( UIWidget, UIMultiLineEdit, NULL )
-{
-	{ NULL, NULL }
+Event EV_Time_Dragged
+(
+	"_umledit_drag_timer",
+	EV_DEFAULT,
+	NULL,
+	NULL,
+	"Used to time mouse drag events"
+);
+Event EV_UIMultiLineEdit_UpdateCvarEvent
+(
+	"updatecvar",
+	EV_DEFAULT,
+	NULL,
+	NULL,
+	"Causes text box to update itself using the assigned cvar"
+);
+Event EV_UIMultiLineEdit_SetEdit
+(
+	"edit",
+	EV_DEFAULT,
+	"b",
+	"bool",
+	"Set whether or not the text is editable"
+);
+
+CLASS_DECLARATION(UIWidget, UIMultiLineEdit, NULL) {
+    {&W_LeftMouseDown,                    &UIMultiLineEdit::MouseDown      },
+    {&W_LeftMouseUp,                      &UIMultiLineEdit::MouseUp        },
+    {&W_LeftMouseDragged,                 &UIMultiLineEdit::MouseDragged   },
+    {&EV_Time_Dragged,                    &UIMultiLineEdit::DragTimer      },
+    {&W_SizeChanged,                      &UIMultiLineEdit::SizeChanged    },
+    {&EV_UIMultiLineEdit_UpdateCvarEvent, &UIMultiLineEdit::UpdateCvarEvent},
+    {&EV_UIMultiLineEdit_SetEdit,         &UIMultiLineEdit::SetEdit        },
+    {NULL,                                NULL                             }
 };
 
 UIMultiLineEdit::UIMultiLineEdit()
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::FrameInitialized
-	(
-	void
-	)
-
+void UIMultiLineEdit::Empty(void)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::PointToSelectionPoint
-	(
-	UIPoint2D& p,
-	selectionpoint_t& sel
-	)
-
+void UIMultiLineEdit::SetEdit(Event *ev)
 {
-	// FIXME: stub
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::setData(const char *data)
+{
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::getData(str& data)
+{
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::FrameInitialized(void)
+{
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::SizeChanged(Event *ev)
+{
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::SortSelection(selectionpoint_t **topsel, selectionpoint_t **botsel)
+{
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::UpdateCvarEvent(Event *ev)
+{
+    // FIXME: stub
+}
+
+void UIMultiLineEdit::Draw(void)
+{
+    // FIXME: stub
 }
 
 static str str_null = "";
 
-str& UIMultiLineEdit::LineFromLineNumber
-	(
-	int num,
-	bool resetpos
-	)
-
+str& UIMultiLineEdit::LineFromLineNumber(int num, bool resetpos)
 {
-	// FIXME: stub
-	return str_null;
+    // FIXME: stub
+    return str_null;
 }
 
-void UIMultiLineEdit::EnsureSelectionPointVisible
-	(
-	selectionpoint_t& point
-	)
-
+void UIMultiLineEdit::PointToSelectionPoint(UIPoint2D& p, selectionpoint_t& sel)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::BoundSelectionPoint
-	(
-	selectionpoint_t& point
-	)
-
+void UIMultiLineEdit::MouseDown(Event *ev)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::SortSelection
-	(
-	selectionpoint_t **topsel,
-	selectionpoint_t **botsel
-	)
-
+void UIMultiLineEdit::MouseUp(Event *ev)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::UpdateCvarEvent
-	(
-	Event *ev
-	)
-
+void UIMultiLineEdit::MouseDragged(Event *ev)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::SetEdit
-	(
-	Event *ev
-	)
-
+void UIMultiLineEdit::DragTimer(Event *ev)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-bool UIMultiLineEdit::IsSelectionEmpty
-	(
-	void
-	)
-
+void UIMultiLineEdit::EnsureSelectionPointVisible(selectionpoint_t& point)
 {
-	// FIXME: stub
-	return false;
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::Draw
-	(
-	void
-	)
-
+void UIMultiLineEdit::BoundSelectionPoint(selectionpoint_t& point)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-qboolean UIMultiLineEdit::KeyEvent
-	(
-	int key,
-	unsigned int time
-	)
-
+qboolean UIMultiLineEdit::KeyEvent(int key, unsigned int time)
 {
-	// FIXME: stub
-	return qfalse;
+    // FIXME: stub
+    return qfalse;
 }
 
-void UIMultiLineEdit::CharEvent
-	(
-	int ch
-	)
-
+void UIMultiLineEdit::DeleteSelection(void)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-UIPoint2D UIMultiLineEdit::getEndSelPoint
-	(
-	void
-	)
-
+void UIMultiLineEdit::CharEvent(int ch)
 {
-	// FIXME: stub
-	return UIPoint2D();
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::MouseDown
-	(
-	Event *ev
-	)
-
+bool UIMultiLineEdit::IsSelectionEmpty(void)
 {
-	// FIXME: stub
+    // FIXME: stub
+    return false;
 }
 
-void UIMultiLineEdit::MouseUp
-	(
-	Event *ev
-	)
-
+void UIMultiLineEdit::CopySelection(void)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::MouseDragged
-	(
-	Event *ev
-	)
-
+void UIMultiLineEdit::PasteSelection(void)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::Scroll
-	(
-	Event *ev
-	)
-
+UIPoint2D UIMultiLineEdit::getEndSelPoint(void)
 {
-	// FIXME: stub
+    // FIXME: stub
+    return UIPoint2D();
 }
 
-void UIMultiLineEdit::DragTimer
-	(
-	Event *ev
-	)
-
+void UIMultiLineEdit::setChanged(bool b)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
 
-void UIMultiLineEdit::SizeChanged
-	(
-	Event *ev
-	)
-
+bool UIMultiLineEdit::IsChanged(void)
 {
-	// FIXME: stub
+    // FIXME: stub
+    return false;
 }
 
-void UIMultiLineEdit::setData
-	(
-	const char *data
-	)
-
+void UIMultiLineEdit::Scroll(Event *ev)
 {
-	// FIXME: stub
+    // FIXME: stub
 }
-
-void UIMultiLineEdit::getData
-	(
-	str& data
-	)
-
-{
-	// FIXME: stub
-}
-
-void UIMultiLineEdit::Empty
-	(
-	void
-	)
-
-{
-	// FIXME: stub
-}
-
-void UIMultiLineEdit::CopySelection
-	(
-	void
-	)
-
-{
-	// FIXME: stub
-}
-
-void UIMultiLineEdit::PasteSelection
-	(
-	void
-	)
-
-{
-	// FIXME: stub
-}
-
-void UIMultiLineEdit::DeleteSelection
-	(
-	void
-	)
-
-{
-	// FIXME: stub
-}
-
-void UIMultiLineEdit::setChanged
-	(
-	bool b
-	)
-
-{
-	// FIXME: stub
-}
-
-bool UIMultiLineEdit::IsChanged
-	(
-	void
-	)
-
-{
-	// FIXME: stub
-	return false;
-}
-
--- code/uilib/uimledit.h
+++ code/uilib/uimledit.h
@@ -20,71 +20,67 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-#ifndef __UIMLEDIT_H__
-#define __UIMLEDIT_H__
+#pragma once
 
 typedef struct selectionpoint_s {
-	int line;
-	int column;
+    int line;
+    int column;
 } selectionpoint_t;
 
 typedef struct selection_s {
-	selectionpoint_s begin;
-	selectionpoint_s end;
+    selectionpoint_s begin;
+    selectionpoint_s end;
 } selection_t;
 
 typedef struct dragState_s {
-	UIPoint2D lastPos;
+    UIPoint2D lastPos;
 } dragState_t;
 
-
-class UIMultiLineEdit : public UIWidget {
+class UIMultiLineEdit : public UIWidget
+{
 protected:
-	selection_s m_selection;
-	dragState_s m_dragState;
-	UList<str> m_lines;
-	UIVertScroll *m_vertscroll;
-	mouseState_t m_mouseState;
-	bool m_shiftForcedDown;
-	bool m_edit;
-	bool m_changed;
+    selection_s   m_selection;
+    dragState_s   m_dragState;
+    UList<str>    m_lines;
+    UIVertScroll *m_vertscroll;
+    mouseState_t  m_mouseState;
+    bool          m_shiftForcedDown;
+    bool          m_edit;
+    bool          m_changed;
 
 public:
-	CLASS_PROTOTYPE( UIMultiLineEdit );
+    CLASS_PROTOTYPE(UIMultiLineEdit);
 
 protected:
-	void	FrameInitialized( void ) override;
-	void	PointToSelectionPoint( UIPoint2D& p, selectionpoint_t& sel );
-	str&	LineFromLineNumber( int num, bool resetpos );
-	void	EnsureSelectionPointVisible( selectionpoint_t& point );
-	void	BoundSelectionPoint( selectionpoint_t& point );
-	void	SortSelection( selectionpoint_t **topsel, selectionpoint_t **botsel );
-	void	UpdateCvarEvent( Event *ev );
-	void	SetEdit( Event *ev );
-	bool	IsSelectionEmpty( void );
+    void FrameInitialized(void) override;
+    void PointToSelectionPoint(UIPoint2D& p, selectionpoint_t& sel);
+    str& LineFromLineNumber(int num, bool resetpos);
+    void EnsureSelectionPointVisible(selectionpoint_t& point);
+    void BoundSelectionPoint(selectionpoint_t& point);
+    void SortSelection(selectionpoint_t **topsel, selectionpoint_t **botsel);
+    void UpdateCvarEvent(Event *ev);
+    void SetEdit(Event *ev);
+    bool IsSelectionEmpty(void);
 
 public:
-	UIMultiLineEdit();
+    UIMultiLineEdit();
 
-	void		Draw( void ) override;
-	qboolean	KeyEvent( int key, unsigned int time ) override;
-	void		CharEvent( int ch ) override;
-	UIPoint2D	getEndSelPoint( void );
-	void		MouseDown( Event *ev );
-	void		MouseUp( Event *ev );
-	void		MouseDragged( Event *ev );
-	void		Scroll( Event *ev );
-	void		DragTimer( Event *ev );
-	void		SizeChanged( Event *ev );
-	void		setData( const char *data );
-	void		getData( str& data );
-	void		Empty( void );
-	void		CopySelection( void );
-	void		PasteSelection( void );
-	void		DeleteSelection( void );
-	void		setChanged( bool b );
-	bool		IsChanged( void );
+    void      Draw(void) override;
+    qboolean  KeyEvent(int key, unsigned int time) override;
+    void      CharEvent(int ch) override;
+    UIPoint2D getEndSelPoint(void);
+    void      MouseDown(Event *ev);
+    void      MouseUp(Event *ev);
+    void      MouseDragged(Event *ev);
+    void      Scroll(Event *ev);
+    void      DragTimer(Event *ev);
+    void      SizeChanged(Event *ev);
+    void      setData(const char *data);
+    void      getData(str& data);
+    void      Empty(void);
+    void      CopySelection(void);
+    void      PasteSelection(void);
+    void      DeleteSelection(void);
+    void      setChanged(bool b);
+    bool      IsChanged(void);
 };
-
-#endif
-
