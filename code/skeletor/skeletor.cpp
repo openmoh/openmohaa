445814b14485d245ecbe133cda267c621c7bef24
smallmodel <15067410+smallmodel@users.noreply.github.com>
2024-06-10 22:15:10 +0200

    Fixed skeletor_c::ChannelNames return value

--- code/skeletor/skeletor.cpp
+++ code/skeletor/skeletor.cpp
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 ChannelNameTable* skeletor_c::ChannelNames()
 {
-    return m_channelNames;
+    return &m_channelNames;
 }
 
 int skelAnimDataGameHeader_s::GetFrameNums(
