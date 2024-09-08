c18cdc78ea259cd00baf0db2c65699187ae9075f
smallmodel <15067410+smallmodel@users.noreply.github.com>
2024-09-08 15:48:46 +0200

    Fix Cgame key catching

--- code/client/cl_keys.cpp
+++ code/client/cl_keys.cpp
@@ -1089,7 +1089,7 @@ void CL_KeyEvent(int key, qboolean down, unsigned time)
 
     if (clc.state > CA_DISCONNECTED && cge && !Key_GetCatcher() && cge->CG_CheckCaptureKey(key, down, time)
         && key != K_ESCAPE) {
-        if (key == K_CONSOLE || (keys[K_SHIFT].down && key == K_ESCAPE)) {
+        if (key != K_CONSOLE && (!keys[K_SHIFT].down || key != K_ESCAPE)) {
             return;
         }
     }
