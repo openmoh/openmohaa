3478bbddf24cad4cd5557193c133fc02a3659f03
smallmodel <15067410+smallmodel@users.noreply.github.com>
2024-09-11 18:02:39 +0200

    Implement RB_ComputeShadowVolume

--- code/renderer/tr_shadows.c
+++ code/renderer/tr_shadows.c
@@ -248,8 +248,102 @@ void RB_ShadowTessEnd( void ) {
 	qglColorMask( GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE );
 }
 
+/*
+=================
+RB_ComputeShadowVolume
+
+=================
+*/
 void RB_ComputeShadowVolume() {
-	// FIXME: unimplemented
+    int i;
+    int numTris;
+    vec3_t lightDir;
+
+    if (r_shadows->integer != 3 || glConfig.stencilBits < 8) {
+        return;
+    }
+
+    VectorSet(lightDir, -2.0, 1.5, 1.0);
+
+    // project vertexes away from light direction
+    for (i = 0; i < tess.numVertexes; i++) {
+        VectorMA(tess.xyz[i], -512, lightDir, tess.xyz[i + tess.numVertexes]);
+    }
+
+    // decide which triangles face the light
+    Com_Memset(numEdgeDefs, 0, 4 * tess.numVertexes);
+
+    numTris = tess.numIndexes / 3;
+    for (i = 0; i < numTris; i++) {
+        int		i1, i2, i3;
+        vec3_t	d1, d2, normal;
+        float* v1, * v2, * v3;
+        float	d;
+
+        i1 = tess.indexes[i * 3 + 0];
+        i2 = tess.indexes[i * 3 + 1];
+        i3 = tess.indexes[i * 3 + 2];
+
+        v1 = tess.xyz[i1];
+        v2 = tess.xyz[i2];
+        v3 = tess.xyz[i3];
+
+        VectorSubtract(v2, v1, d1);
+        VectorSubtract(v3, v1, d2);
+        CrossProduct(d1, d2, normal);
+
+        d = DotProduct(normal, lightDir);
+        if (d > 0) {
+            facing[i] = 1;
+        } else {
+            facing[i] = 0;
+        }
+
+        // create the edges
+        R_AddEdgeDef(i1, i2, facing[i]);
+        R_AddEdgeDef(i2, i3, facing[i]);
+        R_AddEdgeDef(i3, i1, facing[i]);
+    }
+
+	// draw the silhouette edges
+
+	GL_Bind( tr.whiteImage );
+	qglEnable( GL_CULL_FACE );
+	GL_State( GLS_SRCBLEND_ONE | GLS_DSTBLEND_ZERO );
+	qglColor3f( 0.2f, 0.2f, 0.2f );
+
+	// don't write to the color buffer
+	qglColorMask( GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE );
+
+	qglEnable( GL_STENCIL_TEST );
+	qglStencilFunc( GL_ALWAYS, 1, 255 );
+
+	// mirrors have the culling order reversed
+	if ( backEnd.viewParms.isMirror ) {
+		qglCullFace( GL_FRONT );
+		qglStencilOp( GL_KEEP, GL_KEEP, GL_INCR );
+
+		R_RenderShadowEdges();
+
+		qglCullFace( GL_BACK );
+		qglStencilOp( GL_KEEP, GL_KEEP, GL_DECR );
+
+		R_RenderShadowEdges();
+	} else {
+		qglCullFace( GL_BACK );
+		qglStencilOp( GL_KEEP, GL_KEEP, GL_INCR );
+
+		R_RenderShadowEdges();
+
+		qglCullFace( GL_FRONT );
+		qglStencilOp( GL_KEEP, GL_KEEP, GL_DECR );
+
+		R_RenderShadowEdges();
+	}
+
+
+	// reenable writing to the color buffer
+	qglColorMask( GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE );
 }
 
 /*
