79643d963edb935abda67e7237ae37a1a56e5a84
smallmodel <15067410+smallmodel@users.noreply.github.com>
2024-12-15 21:32:23 +0100

    Correctly parse the name of the players returned by the serverstatus command

--- code/client/cl_main.cpp
+++ code/client/cl_main.cpp
@@ -4053,7 +4053,7 @@ CL_ServerStatusResponse
 void CL_ServerStatusResponse( netadr_t from, msg_t *msg ) {
 	const char	*s;
 	char		info[MAX_INFO_STRING];
-	int			i, l, score, ping;
+	int			i, l, ping;
 	int			len;
 	serverStatus_t *serverStatus;
 
@@ -4107,7 +4107,7 @@ void CL_ServerStatusResponse( netadr_t from, msg_t *msg ) {
 
 	if (serverStatus->print) {
 		Com_Printf("\nPlayers:\n");
-		Com_Printf("num: score: ping: name:\n");
+		Com_Printf("num: ping: name:\n");
 	}
 	for (i = 0, s = MSG_ReadStringLine( msg ); *s; s = MSG_ReadStringLine( msg ), i++) {
 
@@ -4115,16 +4115,14 @@ void CL_ServerStatusResponse( netadr_t from, msg_t *msg ) {
 		Com_sprintf(&serverStatus->string[len], sizeof(serverStatus->string)-len, "\\%s", s);
 
 		if (serverStatus->print) {
-			score = ping = 0;
-			sscanf(s, "%d %d", &score, &ping);
+			ping = 0;
+			sscanf(s, "%d", &ping);
 			s = strchr(s, ' ');
-			if (s)
-				s = strchr(s+1, ' ');
 			if (s)
 				s++;
 			else
 				s = "unknown";
-			Com_Printf("%-2d   %-3d    %-3d   %s\n", i, score, ping, s );
+			Com_Printf("%-2d   %-3d   %s\n", i, ping, s );
 		}
 	}
 	len = strlen(serverStatus->string);
