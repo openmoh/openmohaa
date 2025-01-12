59fdcf0ae951b778fbdb280e57db16a5ed8d5bac
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-01-12 20:29:53 +0100

    Add debian 11 and 12

--- docker/dev/build.ps1
+++ docker/dev/build.ps1
@@ -1,7 +1,10 @@
-docker build -t openmohaa/builder.14_04 x86_64/14.04 && `
-docker build -t openmohaa/builder.16_04 x86_64/16.04 && `
-docker build -t openmohaa/builder.18_04 x86_64/18.04 && `
-docker build -t openmohaa/builder.20_04 x86_64/20.04 && `
-docker build -t openmohaa/builder.22_04 x86_64/22.04 && `
-docker build -t openmohaa/builder.22_10 x86_64/22.10 && `
+docker build -t openmohaa/builder.14_04 x86_64/14.04
+docker build -t openmohaa/builder.16_04 x86_64/16.04
+docker build -t openmohaa/builder.18_04 x86_64/18.04
+docker build -t openmohaa/builder.20_04 x86_64/20.04
+docker build -t openmohaa/builder.22_04 x86_64/22.04
+docker build -t openmohaa/builder.22_10 x86_64/22.10
+docker build -t openmohaa/builder.debian-11 x86_64/debian-11
+docker build -t openmohaa/builder.debian-12 x86_64/debian-12
+
 docker build -t openmohaa/builder.18_04.i386 i386/18.04
