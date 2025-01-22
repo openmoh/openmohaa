21779876e1bbc8ce45487f553395d269e4a3b2c4
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-01-22 01:52:05 +0100

    Add code to generate vertices from collision

--- code/fgame/navigation_bsp.cpp
+++ code/fgame/navigation_bsp.cpp
@@ -100,17 +100,19 @@ typedef struct {
 } cshader_t;
 
 typedef struct {
-    cplane_t *plane;
-    int       surfaceFlags;
+    cplane_t  *plane;
+    int        planenum;
+    int        surfaceFlags;
+    winding_t *winding;
 
     dsideequation_t *pEq;
 } cbrushside_t;
 
 typedef struct {
-    int                 contents;
-    vec3_t              bounds[2];
-    int                 numsides;
-    const cbrushside_t *sides;
+    int           contents;
+    vec3_t        bounds[2];
+    int           numsides;
+    cbrushside_t *sides;
 } cbrush_t;
 
 typedef union varnodeUnpacked_u {
@@ -405,6 +407,7 @@ void G_LoadBrushSides(
         num              = LittleLong(in->planeNum);
         shaderNum        = LittleLong(in->shaderNum);
         out.plane        = &planes[num];
+        out.planenum     = num;
         out.surfaceFlags = shaders.ObjectAt(shaderNum + 1).surfaceFlags;
 
         sides.AddObject(out);
@@ -466,8 +469,382 @@ void G_LoadBrushes(
         out.contents = shaders[shaderNum].contentFlags;
 
         G_BoundBrush(&out);
+
+        brushes.AddObject(out);
     }
 }
+
+#define SNAP_EPSILON 0.01
+
+void SnapWeldVector(vec3_t a, vec3_t b, vec3_t out)
+{
+    int   i;
+    vec_t ai, bi, outi;
+
+    /* dummy check */
+    if (a == NULL || b == NULL || out == NULL) {
+        return;
+    }
+
+    /* do each element */
+    for (i = 0; i < 3; i++) {
+        /* round to integer */
+        ai = Q_rint(a[i]);
+        bi = Q_rint(a[i]);
+
+        /* prefer exact integer */
+        if (ai == a[i]) {
+            out[i] = a[i];
+        } else if (bi == b[i]) {
+            out[i] = b[i];
+        }
+
+        /* use nearest */
+        else if (fabs(ai - a[i]) < fabs(bi < b[i])) {
+            out[i] = a[i];
+        } else {
+            out[i] = b[i];
+        }
+
+        /* snap */
+        outi = Q_rint(out[i]);
+        if (fabs(outi - out[i]) <= SNAP_EPSILON) {
+            out[i] = outi;
+        }
+    }
+}
+
+#define DEGENERATE_EPSILON 0.1
+
+qboolean FixWinding(winding_t *w)
+{
+    qboolean valid = qtrue;
+    int      i, j, k;
+    vec3_t   vec;
+    float    dist;
+
+    /* dummy check */
+    if (!w) {
+        return qfalse;
+    }
+
+    /* check all verts */
+    for (i = 0; i < w->numpoints; i++) {
+        /* don't remove points if winding is a triangle */
+        if (w->numpoints == 3) {
+            return valid;
+        }
+
+        /* get second point index */
+        j = (i + 1) % w->numpoints;
+
+        /* degenerate edge? */
+        VectorSubtract(w->p[i], w->p[j], vec);
+        dist = VectorLength(vec);
+        if (dist < DEGENERATE_EPSILON) {
+            valid = qfalse;
+            //Sys_FPrintf( SYS_VRB, "WARNING: Degenerate winding edge found, fixing...\n" );
+
+            /* create an average point (ydnar 2002-01-26: using nearest-integer weld preference) */
+            SnapWeldVector(w->p[i], w->p[j], vec);
+            VectorCopy(vec, w->p[i]);
+            //VectorAdd( w->p[ i ], w->p[ j ], vec );
+            //VectorScale( vec, 0.5, w->p[ i ] );
+
+            /* move the remaining verts */
+            for (k = i + 2; k < w->numpoints; k++) {
+                VectorCopy(w->p[k], w->p[k - 1]);
+            }
+            w->numpoints--;
+        }
+    }
+
+    /* one last check and return */
+    if (w->numpoints < 3) {
+        valid = qfalse;
+    }
+    return valid;
+}
+
+qboolean BoundBrush(cbrush_t& brush)
+{
+    int        i, j;
+    winding_t *w;
+
+    ClearBounds(brush.bounds[0], brush.bounds[1]);
+    for (i = 0; i < brush.numsides; i++) {
+        w = brush.sides[i].winding;
+        if (w == NULL) {
+            continue;
+        }
+        for (j = 0; j < w->numpoints; j++) {
+            AddPointToBounds(w->p[j], brush.bounds[0], brush.bounds[1]);
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        if (brush.bounds[0][i] < MIN_WORLD_COORD || brush.bounds[1][i] > MAX_WORLD_COORD
+            || brush.bounds[0][i] >= brush.bounds[1][i]) {
+            return qfalse;
+        }
+    }
+
+    return qtrue;
+}
+
+/*
+=============
+AllocWinding
+=============
+*/
+winding_t *AllocWinding(int points)
+{
+    winding_t *w;
+    int        s;
+
+    s = sizeof(vec_t) * 3 * points + sizeof(int);
+    w = (winding_t *)gi.Malloc(s);
+    Com_Memset(w, 0, s);
+    return w;
+}
+
+void FreeWinding(winding_t *w)
+{
+    if (*(unsigned *)w == 0xdeaddead) {
+        Com_Error(ERR_FATAL, "FreeWinding: freed a freed winding");
+    }
+    *(unsigned *)w = 0xdeaddead;
+
+    gi.Free(w);
+}
+
+/*
+=================
+BaseWindingForPlane
+=================
+*/
+winding_t *BaseWindingForPlane(vec3_t normal, vec_t dist)
+{
+    int        i, x;
+    vec_t      max, v;
+    vec3_t     org, vright, vup;
+    winding_t *w;
+
+    // find the major axis
+
+    max = -MAP_SIZE;
+    x   = -1;
+    for (i = 0; i < 3; i++) {
+        v = fabs(normal[i]);
+        if (v > max) {
+            x   = i;
+            max = v;
+        }
+    }
+    if (x == -1) {
+        Com_Error(ERR_DROP, "BaseWindingForPlane: no axis found");
+    }
+
+    VectorCopy(vec3_origin, vup);
+    switch (x) {
+    case 0:
+    case 1:
+        vup[2] = 1;
+        break;
+    case 2:
+        vup[0] = 1;
+        break;
+    }
+
+    v = DotProduct(vup, normal);
+    VectorMA(vup, -v, normal, vup);
+    VectorNormalize(vup);
+
+    VectorScale(normal, dist, org);
+
+    CrossProduct(vup, normal, vright);
+
+    VectorScale(vup, MAP_SIZE, vup);
+    VectorScale(vright, MAP_SIZE, vright);
+
+    // project a really big	axis aligned box onto the plane
+    w = AllocWinding(4);
+
+    VectorSubtract(org, vright, w->p[0]);
+    VectorAdd(w->p[0], vup, w->p[0]);
+
+    VectorAdd(org, vright, w->p[1]);
+    VectorAdd(w->p[1], vup, w->p[1]);
+
+    VectorAdd(org, vright, w->p[2]);
+    VectorSubtract(w->p[2], vup, w->p[2]);
+
+    VectorSubtract(org, vright, w->p[3]);
+    VectorSubtract(w->p[3], vup, w->p[3]);
+
+    w->numpoints = 4;
+
+    return w;
+}
+
+/*
+=============
+ChopWindingInPlace
+=============
+*/
+void ChopWindingInPlace(winding_t **inout, vec3_t normal, vec_t dist, vec_t epsilon)
+{
+    winding_t   *in;
+    vec_t        dists[MAX_POINTS_ON_WINDING + 4];
+    int          sides[MAX_POINTS_ON_WINDING + 4];
+    int          counts[3];
+    static vec_t dot; // VC 4.2 optimizer bug if not static
+    int          i, j;
+    vec_t       *p1, *p2;
+    vec3_t       mid;
+    winding_t   *f;
+    int          maxpts;
+
+    in        = *inout;
+    counts[0] = counts[1] = counts[2] = 0;
+
+    // determine sides for each point
+    for (i = 0; i < in->numpoints; i++) {
+        dot = DotProduct(in->p[i], normal);
+        dot -= dist;
+        dists[i] = dot;
+        if ((dot + 0.0005) > epsilon) {
+            sides[i] = SIDE_FRONT;
+        } else if ((dot - 0.0005) < -epsilon) {
+            sides[i] = SIDE_BACK;
+        } else {
+            sides[i] = SIDE_ON;
+        }
+        counts[sides[i]]++;
+    }
+    sides[i] = sides[0];
+    dists[i] = dists[0];
+
+    if (!counts[0]) {
+        FreeWinding(in);
+        *inout = NULL;
+        return;
+    }
+    if (!counts[1]) {
+        return; // inout stays the same
+    }
+
+    maxpts = in->numpoints + 4; // cant use counts[0]+2 because
+    // of fp grouping errors
+
+    f = AllocWinding(maxpts);
+
+    for (i = 0; i < in->numpoints; i++) {
+        p1 = in->p[i];
+
+        if (sides[i] == SIDE_ON) {
+            VectorCopy(p1, f->p[f->numpoints]);
+            f->numpoints++;
+            continue;
+        }
+
+        if (sides[i] == SIDE_FRONT) {
+            VectorCopy(p1, f->p[f->numpoints]);
+            f->numpoints++;
+        }
+
+        if (sides[i + 1] == SIDE_ON || sides[i + 1] == sides[i]) {
+            continue;
+        }
+
+        // generate a split point
+        p2 = in->p[(i + 1) % in->numpoints];
+
+        dot = dists[i] / (dists[i] - dists[i + 1]);
+        for (j = 0; j < 3; j++) { // avoid round off error when possible
+            if (normal[j] == 1) {
+                mid[j] = dist;
+            } else if (normal[j] == -1) {
+                mid[j] = -dist;
+            } else {
+                mid[j] = p1[j] + dot * (p2[j] - p1[j]);
+            }
+        }
+
+        VectorCopy(mid, f->p[f->numpoints]);
+        f->numpoints++;
+    }
+
+    if (f->numpoints > maxpts) {
+        Com_Error(ERR_DROP, "ClipWinding: points exceeded estimate");
+    }
+    if (f->numpoints > MAX_POINTS_ON_WINDING) {
+        Com_Error(ERR_DROP, "ClipWinding: MAX_POINTS_ON_WINDING");
+    }
+
+    FreeWinding(in);
+    *inout = f;
+}
+
+qboolean CreateBrushWindings(const Container<cplane_t>& planes, cbrush_t& brush)
+{
+    int           i, j;
+    winding_t    *w;
+    cbrushside_t *side;
+    cplane_t     *plane;
+
+    /* walk the list of brush sides */
+    for (i = 0; i < brush.numsides; i++) {
+        /* get side and plane */
+        side  = &brush.sides[i];
+        plane = side->plane;
+
+        /* make huge winding */
+        w = BaseWindingForPlane(plane->normal, plane->dist);
+
+        /* walk the list of brush sides */
+        for (j = 0; j < brush.numsides && w != NULL; j++) {
+            if (i == j) {
+                continue;
+            }
+            if (brush.sides[j].planenum == (brush.sides[i].planenum ^ 1)) {
+                continue; /* back side clipaway */
+            }
+            plane = (cplane_t *)&planes[brush.sides[j].planenum ^ 1];
+            ChopWindingInPlace(&w, plane->normal, plane->dist, 0); // CLIP_EPSILON );
+
+            FixWinding(w);
+        }
+
+        /* set side winding */
+        side->winding = w;
+    }
+
+    /* find brush bounds */
+    return BoundBrush(brush);
+}
+
+#define COLINEAR_AREA 10
+
+static qboolean IsTriangleDegenerate(const vec3_t *points, int a, int b, int c)
+{
+    vec3_t v1, v2, v3;
+    float  d;
+
+    VectorSubtract(points[b], points[a], v1);
+    VectorSubtract(points[c], points[a], v2);
+    CrossProduct(v1, v2, v3);
+    d = VectorLength(v3);
+
+    // assume all very small or backwards triangles will cause problems
+    if (d < COLINEAR_AREA) {
+        return qtrue;
+    }
+
+    return qfalse;
+}
+
+#define MAX_INDEXES 1024
 
 /*
 ============
@@ -486,13 +863,109 @@ void G_GenerateVerticesFromHull(
     Container<cplane_t>     planes;
     Container<cbrushside_t> sides;
     Container<cbrush_t>     brushes;
+    size_t                  i, j, k;
+    size_t                  baseVertex;
 
     G_LoadShaders(shadersLump, shaders);
     G_LoadPlanes(planesLump, planes);
     G_LoadBrushSides(brushSidesLump, shaders, planes, sides);
     G_LoadBrushes(brushesLump, shaders, sides, brushes);
 
-    // FIXME: generate vertices
+    for (i = 1; i <= brushes.NumObjects(); i++) {
+        cbrush_t& brush = brushes.ObjectAt(i);
+        CreateBrushWindings(planes, brush);
+
+        for (j = 0; j < brush.numsides; j++) {
+            cbrushside_t& side = brush.sides[j];
+            int           indexes[MAX_INDEXES];
+
+            if (!side.winding) {
+                continue;
+            }
+
+            baseVertex = navMap.vertices.NumObjects();
+
+            for (k = 0; k < side.winding->numpoints; k++) {
+                navMap.vertices.AddObject(side.winding->p[k]);
+            }
+
+            if (side.winding->numpoints == 3) {
+                navMap.indices.AddObject(baseVertex + 0);
+                navMap.indices.AddObject(baseVertex + 1);
+                navMap.indices.AddObject(baseVertex + 2);
+                continue;
+            }
+
+            int least = 0;
+            for (k = 0; k < side.winding->numpoints; k++) {
+                /* get points */
+                const vec_t *v1 = side.winding->p[k];
+                const vec_t *v2 = side.winding->p[least];
+
+                /* compare */
+                if (v1[0] < v2[0] || (v1[0] == v2[0] && v1[1] < v2[1])
+                    || (v1[0] == v2[0] && v1[1] == v2[1] && v1[2] < v2[2])) {
+                    least = k;
+                }
+            }
+
+            /* determine the triangle strip order */
+            size_t numIndexes = (side.winding->numpoints - 2) * 3;
+            size_t ni         = 0;
+
+            /* try all possible orderings of the points looking for a non-degenerate strip order */
+            for (int r = 0; r < side.winding->numpoints; r++) {
+                /* set rotation */
+                int rotate = (r + least) % side.winding->numpoints;
+
+                /* walk the winding in both directions */
+                for (ni = 0, k = 0; k < side.winding->numpoints - 2 - k; k++) {
+                    /* make indexes */
+                    int a = (side.winding->numpoints - 1 - k + rotate) % side.winding->numpoints;
+                    int b = (k + rotate) % side.winding->numpoints;
+                    int c = (side.winding->numpoints - 2 - k + rotate) % side.winding->numpoints;
+
+                    /* test this triangle */
+                    if (side.winding->numpoints > 4 && IsTriangleDegenerate(side.winding->p, a, b, c)) {
+                        break;
+                    }
+                    indexes[ni++] = a;
+                    indexes[ni++] = b;
+                    indexes[ni++] = c;
+
+                    /* handle end case */
+                    if (k + 1 != side.winding->numpoints - 1 - k) {
+                        /* make indexes */
+                        a = (side.winding->numpoints - 2 - k + rotate) % side.winding->numpoints;
+                        b = (k + rotate) % side.winding->numpoints;
+                        c = (k + 1 + rotate) % side.winding->numpoints;
+
+                        /* test triangle */
+                        if (side.winding->numpoints > 4 && IsTriangleDegenerate(side.winding->p, a, b, c)) {
+                            break;
+                        }
+                        indexes[ni++] = a;
+                        indexes[ni++] = b;
+                        indexes[ni++] = c;
+                    }
+                }
+
+                /* valid strip? */
+                if (ni == numIndexes) {
+                    break;
+                }
+            }
+
+            /* if any triangle in the strip is degenerate, render from a centered fan point instead */
+            if (ni < numIndexes) {
+                return;
+            }
+
+            for (k = 0; k < numIndexes; k++) {
+                navMap.indices.AddObject(baseVertex + indexes[k]);
+            }
+        }
+    }
 }
 
 /*
@@ -2138,7 +2611,7 @@ void G_Navigation_ProcessBSPForNavigation(const char *mapname, navMap_t& outNavi
         lumps[1] = gameLump_c::LoadLump(h, *Q_GetLumpByVersion(&header, LUMP_PLANES));
         lumps[2] = gameLump_c::LoadLump(h, *Q_GetLumpByVersion(&header, LUMP_BRUSHSIDES));
         lumps[3] = gameLump_c::LoadLump(h, *Q_GetLumpByVersion(&header, LUMP_BRUSHES));
-        G_GenerateVerticesFromHull(outNavigationMap, lumps[0], lumps[1], lumps[2], lumps[3]);
+        //G_GenerateVerticesFromHull(outNavigationMap, lumps[0], lumps[1], lumps[2], lumps[3]);
 
         // Gather vertices from meshes and surfaces
         lumps[0] = gameLump_c::LoadLump(h, *Q_GetLumpByVersion(&header, LUMP_SURFACES));
