70f6ede7b8609348da12e5825ed2bc3d78e20e86
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-03-01 00:09:29 +0100

    Add GAMESPY_PORT environment variable

--- docker/server/base/entrypoint.sh
+++ docker/server/base/entrypoint.sh
@@ -2,4 +2,4 @@
 
 # It's amazing that with Docker, one can't put environment variables in the array of the ENTRYPOINT command
 
-/usr/local/games/openmohaa/lib/openmohaa/omohaaded +set fs_homepath home +set dedicated 2 +set net_port $GAME_PORT:-12203 $@
+/usr/local/games/openmohaa/lib/openmohaa/omohaaded +set fs_homepath home +set dedicated 2 +set net_port $GAME_PORT:-12203 +set net_gamespy_port $GAMESPY_PORT:-12300 $@
