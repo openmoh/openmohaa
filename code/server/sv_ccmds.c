e28f722c8522382230643de4bd29bd142201d903
ysdragon <youssefelkholey@gmail.com>
2025-03-09 15:45:19 +0200

    Add kick command with reason and enhance ban functionality to ban with reason

--- code/server/sv_ccmds.c
+++ code/server/sv_ccmds.c
@@ -387,6 +387,12 @@ static void SV_Kick_f( void ) {
 		return;
 	}
 
+	// Send the kick message to the client
+	NET_OutOfBandPrint( NS_SERVER, cl->netchan.remoteAddress, "droperror\nKicked from server" );
+
+	// Print the kick to all clients
+	SV_SendServerCommand( NULL, "print \"" HUD_MESSAGE_WHITE "%s was kicked\n\"", cl->name );
+
 	SV_DropClient( cl, "was kicked" );
 	cl->lastPacketTime = svs.time;	// in case there is a funny zombie
 }
@@ -451,8 +457,59 @@ static void SV_KickNum_f( void ) {
 		return;
 	}
 
+	// Send the kick message to the client
+	SV_NET_OutOfBandPrint(&svs.netprofile, cl->netchan.remoteAddress, "droperror\nKicked from server");
+	
+	// Print the kick to all clients	
+	SV_SendServerCommand( NULL, "print \"" HUD_MESSAGE_WHITE "%s was kicked\n\"", cl->name );
+	
 	SV_DropClient( cl, "was kicked" );
 	cl->lastPacketTime = svs.time;	// in case there is a funny zombie
+}
+
+/*
+==================
+SV_KickNumr_f
+
+Kick a user off of the server with a reason
+==================
+*/
+static void SV_KickNumr_f( void ) {
+	client_t	*cl;
+	char        reason[MAX_STRING_CHARS];
+
+	// make sure server is running
+	if ( !com_sv_running->integer ) {
+		Com_Printf( "Server is not running.\n" );
+		return;
+	}
+
+	if ( Cmd_Argc() < 3 ) {
+		Com_Printf ("Usage: %s <client number> <reason>\n", Cmd_Argv(0));
+		return;
+	}
+
+	cl = SV_GetPlayerByNum();
+	if ( !cl ) {
+		return;
+	}
+	if( cl->netchan.remoteAddress.type == NA_LOOPBACK ) {
+		Com_Printf("Cannot kick host player\n");
+		return;
+	}
+
+	// Get the reason from remaining arguments
+	Q_strncpyz(reason, Cmd_ArgsFrom(2), sizeof(reason));
+
+	// Send the kick reason to the client
+	SV_NET_OutOfBandPrint(&svs.netprofile, cl->netchan.remoteAddress, "droperror\nKicked from server for:\n%s", reason);
+
+	// Print the kick to all clients
+	SV_SendServerCommand(NULL, "print \"" HUD_MESSAGE_WHITE "%s was kicked for %s\n\"", cl->name, reason);
+
+	// Actually drop the client
+	SV_DropClient( cl, va("was kicked for %s", reason) );
+	cl->lastPacketTime = svs.time;
 }
 
 #ifndef STANDALONE
@@ -771,11 +828,13 @@ Ban a user from being able to play on this server based on his ip address.
 static void SV_AddBanToList(qboolean isexception)
 {
 	char *banstring;
+	char *reason = NULL;
 	char addy2[NET_ADDRSTRMAXLEN];
 	netadr_t ip;
 	int index, argc, mask;
 	serverBan_t *curban;
-
+	client_t *cl = NULL;
+	
 	// make sure server is running
 	if ( !com_sv_running->integer ) {
 		Com_Printf( "Server is not running.\n" );
@@ -784,9 +843,9 @@ static void SV_AddBanToList(qboolean isexception)
 
 	argc = Cmd_Argc();
 	
-	if(argc < 2 || argc > 3)
+	if(argc < 2)
 	{
-		Com_Printf ("Usage: %s (ip[/subnet] | clientnum [subnet])\n", Cmd_Argv(0));
+		Com_Printf ("Usage: %s (ip[/subnet] | clientnum [subnet] [\"reason\"])\n", Cmd_Argv(0));
 		return;
 	}
 
@@ -798,10 +857,14 @@ static void SV_AddBanToList(qboolean isexception)
 
 	banstring = Cmd_Argv(1);
 	
+	// Get optional reason - can be at arg 2 for IP bans or arg 3 for client num bans
 	if(strchr(banstring, '.') || strchr(banstring, ':'))
 	{
-		// This is an ip address, not a client num.
-		
+		// IP address ban 
+		if(argc >= 3) {
+			reason = Cmd_ArgsFrom(2);
+		}
+
 		if(SV_ParseCIDRNotation(&ip, &mask, banstring))
 		{
 			Com_Printf("Error: Invalid address %s\n", banstring);
@@ -810,10 +873,7 @@ static void SV_AddBanToList(qboolean isexception)
 	}
 	else
 	{
-		client_t *cl;
-		
-		// client num.
-		
+		// client num ban
 		cl = SV_GetPlayerByNum();
 
 		if(!cl)
@@ -824,23 +884,36 @@ static void SV_AddBanToList(qboolean isexception)
 		
 		ip = cl->netchan.remoteAddress;
 		
-		if(argc == 3)
+		if(argc >= 3)
 		{
-			mask = atoi(Cmd_Argv(2));
+			if(strchr(Cmd_Argv(2), '.') || strchr(Cmd_Argv(2), ':')) {
+				// Second arg is subnet mask
+				mask = atoi(Cmd_Argv(2));
 			
-			if(ip.type == NA_IP)
-			{
-				if(mask < 1 || mask > 32)
-					mask = 32;
-			}
-			else
-			{
-				if(mask < 1 || mask > 128)
-					mask = 128;
+				if(ip.type == NA_IP)
+				{
+					if(mask < 1 || mask > 32)
+						mask = 32;
+				}
+				else
+				{
+					if(mask < 1 || mask > 128)
+						mask = 128;
+				}
+
+				// Get optional reason after subnet
+				if(argc >= 4) {
+					reason = Cmd_ArgsFrom(3);
+				}
+			} else {
+				// Second arg is start of reason
+				reason = Cmd_ArgsFrom(2);
+				mask = (ip.type == NA_IP6) ? 128 : 32;
 			}
 		}
-		else
+		else {
 			mask = (ip.type == NA_IP6) ? 128 : 32;
+		}
 	}
 
 	if(ip.type != NA_IP && ip.type != NA_IP6)
@@ -900,8 +973,37 @@ static void SV_AddBanToList(qboolean isexception)
 	
 	SV_WriteBans();
 
-	Com_Printf("Added %s: %s/%d\n", isexception ? "ban exception" : "ban",
-		   NET_AdrToString(ip), mask);
+	// Find and kick any connected clients matching the banned IP
+	for(index = 0; index < sv_maxclients->integer; index++) {
+		client_t *kickcl = &svs.clients[index];
+		
+		if(!kickcl->state)
+			continue;
+
+		if(NET_CompareBaseAdrMask(kickcl->netchan.remoteAddress, ip, mask)) {
+			if(reason) {
+				SV_NET_OutOfBandPrint(&svs.netprofile, kickcl->netchan.remoteAddress,
+					"droperror\nYou have been banned from this server for:\n%s", reason);
+				
+				SV_SendServerCommand(NULL, "print \"" HUD_MESSAGE_WHITE "%s was banned for %s\n\"", cl->name, reason);
+				
+				SV_DropClient(kickcl, va("was banned for %s", reason));
+			} else {
+				SV_NET_OutOfBandPrint(&svs.netprofile, kickcl->netchan.remoteAddress,
+					"droperror\nYou have been banned from this server");
+
+				SV_SendServerCommand(NULL, "print \"" HUD_MESSAGE_WHITE "%s was banned\n\"", cl->name);
+				
+				SV_DropClient(kickcl, "was banned");
+			}
+			kickcl->lastPacketTime = svs.time;
+		}
+	}
+
+	Com_Printf("Added %s: %s/%d%s%s\n", isexception ? "ban exception" : "ban",
+		   NET_AdrToString(ip), mask,
+		   reason ? " with reason: " : "",
+		   reason ? reason : "");
 }
 
 /*
@@ -1816,6 +1918,8 @@ void SV_AddOperatorCommands(void) {
 	Cmd_AddCommand("kickall", SV_KickAll_f);
 	Cmd_AddCommand("kicknum", SV_KickNum_f);
 	Cmd_AddCommand("clientkick", SV_KickNum_f); // Legacy command
+	Cmd_AddCommand("clientkickr", SV_KickNumr_f); // Legacy command with reason
+	Cmd_AddCommand("kickr", SV_KickNumr_f); // Legacy command with reason
 	Cmd_AddCommand("status", SV_Status_f);
 	Cmd_AddCommand("serverinfo", SV_Serverinfo_f);
 	Cmd_AddCommand("systeminfo", SV_Systeminfo_f);
@@ -1846,6 +1950,10 @@ void SV_AddOperatorCommands(void) {
 	Cmd_AddCommand("rehashbans", SV_RehashBans_f);
 	Cmd_AddCommand("listbans", SV_ListBans_f);
 	Cmd_AddCommand("banaddr", SV_BanAddr_f);
+	Cmd_AddCommand("banip", SV_BanAddr_f);
+	Cmd_AddCommand("banid", SV_BanAddr_f);
+	Cmd_AddCommand("banipr", SV_BanAddr_f); 
+	Cmd_AddCommand("banidr", SV_BanAddr_f);
 	Cmd_AddCommand("exceptaddr", SV_ExceptAddr_f);
 	Cmd_AddCommand("bandel", SV_BanDel_f);
 	Cmd_AddCommand("exceptdel", SV_ExceptDel_f);
