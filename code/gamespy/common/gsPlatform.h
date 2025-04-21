aa7639485eff3ffd966a8332b1b3fc1253f741bd
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-04-22 00:16:07 +0200

    Use __linux__ definition to check for linux

--- code/gamespy/common/gsPlatform.h
+++ code/gamespy/common/gsPlatform.h
@@ -23,7 +23,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 // Set the platform define
-#if defined(_LINUX) || defined(_MACOSX) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#if defined (__linux__) || defined(_LINUX) || defined(_MACOSX) || defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
 	#define _UNIX
 #elif __mips64
 	#ifndef _PS2
