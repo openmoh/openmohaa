62a52c8974e88e45693cc641c6281c00ff1c20b9
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-06-04 01:10:18 +0200

    Set agent max height to the character Z bounding box

--- code/fgame/navigation_recast_config.h
+++ code/fgame/navigation_recast_config.h
@@ -33,7 +33,7 @@ namespace NavigationMapConfiguration
 {
     static const float recastCellSize   = 10.25;
     static const float recastCellHeight = 1.0;
-    static const float agentHeight      = DEFAULT_VIEWHEIGHT;
+    static const float agentHeight      = MAXS_Z;
     static const float agentMaxClimb    = STEPSIZE;
 
     // normal of { 0.714142799, 0, 0.700000048 }, or an angle of -44.4270058
