fabd2fd848727babb0d1ab35ccd9973a2a12865a
Tim Angus <tim@ngus.net>
2025-07-30 12:05:23 +0100

    Fix incorrect architecture detection on Windows
    Co-authored-by: smallmodel <15067410+smallmodel@users.noreply.github.com>

--- code/qcommon/q_platform.h
+++ code/qcommon/q_platform.h
@@ -125,12 +125,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define ID_INLINE __inline
 #define PATH_SEP '\\'
 
-#if defined(_WIN64) || defined( __WIN64__ ) 
 #if defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(_M_X64)
 #  define ARCH_STRING "x86_64"
 #elif defined(__aarch64__) || defined(__ARM64__) || defined (_M_ARM64)
 #  define ARCH_STRING "arm64"
-#endif
 #elif defined _M_ALPHA
 #  define ARCH_STRING "AXP"
 #endif
@@ -159,8 +157,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #if defined( _M_IX86 ) || defined( __i386__ )
 #define ARCH_STRING "x86"
-#elif defined _M_ALPHA
-#define ARCH_STRING "AXP"
 #endif
 
 #define Q3_LITTLE_ENDIAN
