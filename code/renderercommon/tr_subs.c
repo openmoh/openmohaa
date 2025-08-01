b0ca50c4c54bbb0889173fb0faf7d190e67eeed1
Tim Angus <tim@ngus.net>
2025-08-01 20:21:10 +0100

    Merge identical tr_subs.c into renderercommon

--- code/renderercommon/tr_subs.c
+++ code/renderercommon/tr_subs.c
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 // tr_subs.c - common function replacements for modular renderer
 
-#include "tr_local.h"
+#include "tr_common.h"
 
 void QDECL Com_Printf( const char *msg, ... )
 {
@@ -33,6 +33,18 @@ void QDECL Com_Printf( const char *msg, ... )
 	va_end(argptr);
 
 	ri.Printf(PRINT_ALL, "%s", text);
+}
+
+void QDECL Com_DPrintf( const char *msg, ... )
+{
+	va_list         argptr;
+	char            text[1024];
+
+	va_start(argptr, msg);
+	Q_vsnprintf(text, sizeof(text), msg, argptr);
+	va_end(argptr);
+
+	ri.Printf(PRINT_DEVELOPER, "%s", text);
 }
 
 void QDECL Com_Error( int level, const char *error, ... )
