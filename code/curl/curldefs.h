5ed0455215a7f3a3be74c95d478e908a3f5b71ce
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-08-07 22:33:58 +0200

    Fix curl builds

--- code/curl/curldefs.h
+++ code/curl/curldefs.h
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "../qcommon/q_shared.h"
 
-#ifdef HAS_LIBCURL
+#ifdef USE_HTTP
 
 #    include <stdint.h>
 #    include <stdlib.h>
