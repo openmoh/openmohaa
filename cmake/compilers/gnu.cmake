2adabc746861fd6463fe12ed56ad26b3a94ec597
Tim Angus <tim@ngus.net>
2025-08-19 10:46:16 +0100

    Add upstream GNU changes
    Co-authored-by: smallmodel <15067410+smallmodel@users.noreply.github.com>

--- cmake/compilers/gnu.cmake
+++ cmake/compilers/gnu.cmake
@@ -7,8 +7,10 @@ endif()
 enable_language(ASM)
 
 set(ASM_SOURCES
-    ${SOURCE_DIR}/asm/snapvector.c
     ${SOURCE_DIR}/asm/ftola.c
+    ${SOURCE_DIR}/asm/matha.S
+    ${SOURCE_DIR}/asm/snapvector.c
+    ${SOURCE_DIR}/asm/snd_mixa.S
 )
 
 add_compile_options(-Wall -Wimplicit
@@ -16,8 +18,19 @@ add_compile_options(-Wall -Wimplicit
     -Wstrict-aliasing=2 -Wmissing-format-attribute
     -Wdisabled-optimization -Werror-implicit-function-declaration)
 
-add_compile_options(-Wno-strict-aliasing
-    -Wno-format-zero-length -Wno-format-nonliteral)
+add_compile_options(-Wno-format-zero-length -Wno-format-nonliteral)
+
+# There are lots of instances of union based aliasing in the code
+# that rely on the compiler not optimising them away, so disable it
+add_compile_options(-fno-strict-aliasing)
+
+# This is necessary to hide all symbols unless explicitly exported
+# via the Q_EXPORT macro
+#add_compile_options(-fvisibility=hidden)
+
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    set(CMAKE_INSTALL_PREFIX /opt/mohaa)
+endif()
 
 add_compile_options(-Wno-comment)
 # Treat no return type as error
