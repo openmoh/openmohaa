1c27f3edd1aa5dde9bb88a662f04ccf09e5f99e3
Tim Angus <tim@ngus.net>
2025-10-03 20:17:20 +0100

    Make sound channel exhaustion warning a Com_DPrintf

--- code/client/snd_dma.c
+++ code/client/snd_dma.c
@@ -621,7 +621,7 @@ static void S_Base_StartSoundEx( vec3_t origin, int entityNum, int entchannel, s
 					}
 				}
 				if (chosen == -1) {
-					Com_Printf("dropping sound\n");
+					Com_DPrintf("dropping sound\n");
 					return;
 				}
 			}
