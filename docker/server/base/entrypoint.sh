9077a8c0eee56ce18efc6524f35881fa1993cd64
smallmodel <15067410+smallmodel@users.noreply.github.com>
2025-11-23 19:02:32 +0100

    fix(container): fix varibale usage in the entry point

--- docker/server/base/entrypoint.sh
+++ docker/server/base/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# It's amazing that with Docker, one can't put environment variables in the array of the ENTRYPOINT command
-
-/usr/local/games/openmohaa/lib/openmohaa/omohaaded +set fs_homepath home +set dedicated 2 +set net_port $GAME_PORT:-12203 +set net_gamespy_port $GAMESPY_PORT:-12300 $@
+/usr/local/games/openmohaa/lib/openmohaa/omohaaded +set fs_homepath home +set dedicated 2 +set net_port ${GAME_PORT:-12203} +set net_gamespy_port ${GAMESPY_PORT:-12300} $@
